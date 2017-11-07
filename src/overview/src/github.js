c3dfb41215db554e385ce226dc22bcd81bed4b8a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-11-07 23:32:59 +0530

    make linter happy

--- src/overview/src/github.js
+++ src/overview/src/github.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import Container from './container'
-
 const Github = () => (
-  <a href="https://github.com/auth0/cosmos" target="_blank" style={{ display: 'block', textAlign: 'right' }}>
-    <img src="/github.png" />
+  <a
+    href="https://github.com/auth0/cosmos"
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{ display: 'block', textAlign: 'right' }}>
+    <img alt="github" src="/github.png" />
   </a>
 )
 
