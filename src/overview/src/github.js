d6b5fd4521ac20f67660c3c50fed5de2ee627bf5
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-11-07 23:32:12 +0530

    add link to repo on overview

---
+++ src/overview/src/github.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Container from './container'
+
+const Github = () => (
+  <a href="https://github.com/auth0/cosmos" target="_blank" style={{ display: 'block', textAlign: 'right' }}>
+    <img src="/github.png" />
+  </a>
+)
+
+export default Github
