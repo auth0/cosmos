df72b5c4d2f7d01545acaeea5794f7d3d4668e1f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-11-10 00:03:32 +0530

    only deploy master

--- tooling/deploy.js
+++ tooling/deploy.js
@@ -5,13 +5,11 @@ const { event, branch } = require('ci-env')
 const githubToken = process.env.github_token
 const nowToken = process.env.NOW_TOKEN
 
-if (event === 'pull_request' || branch === 'master') {
+if (branch === 'master') {
   exec(`./node_modules/.bin/now-travis --file=staging-url`, { stdio: [0, 1, 2] })
   const stagingUrl = fs.readFileSync('./staging-url', 'utf8')
 
-  let alias
-  if (branch === 'master') alias = 'cosmos'
-  else alias = 'cosmos-' + branch
+  let alias = 'cosmos'
 
   exec(`now-replace -t ${nowToken} ${alias} ${stagingUrl}`, { stdio: [0, 1, 2] })
 }
