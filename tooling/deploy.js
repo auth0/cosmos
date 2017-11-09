eb1b2abf5ae4263cda1e8cdbf39e7c4b69cd0d88
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-11-09 22:17:20 +0530

    remove alias for master

--- tooling/deploy.js
+++ tooling/deploy.js
@@ -5,11 +5,14 @@ const { event, branch } = require('ci-env')
 const githubToken = process.env.github_token
 const nowToken = process.env.NOW_TOKEN
 
-if (event === 'pull_request') {
+if (event === 'pull_request' || branch === 'master') {
   exec(`./node_modules/.bin/now-travis --file=staging-url`, { stdio: [0, 1, 2] })
   const stagingUrl = fs.readFileSync('./staging-url', 'utf8')
 
-  const alias = 'cosmos-' + branch
+  let alias
+  if (branch === 'master') alias = 'cosmos'
+  else alias = 'cosmos-' + branch
+
   exec(`now-replace -t ${nowToken} ${alias} ${stagingUrl}`, { stdio: [0, 1, 2] })
 }
 
