0dc7a945ed61cab245c689d28aae1eade64f8174
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-14 10:54:52 -0300

    use the first system component

--- src/manage/index.js
+++ src/manage/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-import { Dummy as Navigation, Dummy as Sidebar, Dummy as Main } from './dummy-components'
+import { Dummy as Navigation, Dummy as Sidebar } from './dummy-components'
+import { Heading1 } from '../components'
+
+const Main = () => (
+  <div>
+    <Heading1>Default App</Heading1>
+  </div>
+)
 
 export default () => (
   <div>
