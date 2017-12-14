a0683a5acc3855395acd505f90c407e2c90c833e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-14 12:08:37 -0300

    make docs really live

--- tooling/component-metadata.js
+++ tooling/component-metadata.js
@@ -2,21 +2,37 @@ const fs = require('fs')
 const glob = require('glob')
 const { parse, defaultHandlers } = require('react-docgen')
 const { createDisplayNameHandler } = require('react-docgen-displayname-handler')
+const path = require('path')
+const chokidar = require('chokidar')
 
-const metadata = glob
-  .sync('src/components/atoms/*.js')
-  .map(path => ({
-    filepath: path,
-    source: fs.readFileSync(path, 'utf8')
-  }))
-  .map(({ source, filepath }) => {
-    try {
-      const handlers = defaultHandlers.concat(createDisplayNameHandler(filepath))
-      return parse(source, null, handlers)
-    } catch (err) {
-      console.log('Could not parse component:' + filepath)
-    }
-  })
-  .filter(meta => meta)
+const watch = process.argv.includes('-w') || process.argv.includes('--watch')
 
-fs.writeFileSync('src/docs/metadata.json', JSON.stringify({ metadata }, null, 2), 'utf8')
+const run = () => {
+  console.log('Updading docs')
+  const metadata = glob
+    .sync('src/components/atoms/*.js')
+    .map(path => ({
+      filepath: path,
+      source: fs.readFileSync(path, 'utf8')
+    }))
+    .map(({ source, filepath }) => {
+      try {
+        const handlers = defaultHandlers.concat(createDisplayNameHandler(filepath))
+        return parse(source, null, handlers)
+      } catch (err) {
+        console.log('Could not parse component:' + filepath)
+      }
+    })
+    .filter(meta => meta)
+
+  fs.writeFileSync('src/docs/metadata.json', JSON.stringify({ metadata }, null, 2), 'utf8')
+}
+
+if (watch) {
+  console.log('running in watch mode')
+  chokidar
+    .watch([path.join(__dirname, '../src/**/*.js')], { ignored: ['node_modules'] })
+    .on('ready', run)
+    .on('change', run)
+    .on('unlink', run)
+}
