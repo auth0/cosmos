04a8b3ec27f058859a574a3b5f981ad68f75f8cb
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-20 10:57:59 -0300

    move icon data to config file

--- src/components/atoms/icon.js
+++ src/components/atoms/icon.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled, { injectGlobal } from 'styled-components'
 import PropTypes from 'prop-types'
 
-import data from './icons.json'
+import iconData from './icons.json'
 
 injectGlobal`
   @font-face {
@@ -18,114 +18,14 @@ const StyledIcon = styled.i`
   font-size: ${props => props.size}px;
   font-weight: 400;
   font-style: normal;
-`
-
-const Success = styled(StyledIcon)`
   &:after {
-    content: '';
-  }
-`
-
-const Clients = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const HourGlass = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Extensions = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Trash = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Notifications = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Multifactor = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Reveal = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Dashboard = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-const ArrowRight = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Users = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Connections = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Analytics = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Emails = styled(StyledIcon)`
-  &:after {
-    content: '';
-  }
-`
-
-const Copy = styled(StyledIcon)`
-  &:after {
-    content: '';
+    content: '${props => props.icon.content}';
   }
 `
 
 const Icon = props => {
-  if (props.type === 'success') return <Success {...props} />
-  else if (props.type === 'clients') return <Clients {...props} />
-  else if (props.type === 'hourglass') return <HourGlass {...props} />
-  else if (props.type === 'extensions') return <Extensions {...props} />
-  else if (props.type === 'trash') return <Trash {...props} />
-  else if (props.type === 'notifications') return <Notifications {...props} />
-  else if (props.type === 'multifactor') return <Multifactor {...props} />
-  else if (props.type === 'reveal') return <Reveal {...props} />
-  else if (props.type === 'dashboard') return <Dashboard {...props} />
-  else if (props.type === 'arrow-right') return <ArrowRight {...props} />
-  else if (props.type === 'users') return <Users {...props} />
-  else if (props.type === 'connections') return <Connections {...props} />
-  else if (props.type === 'analytics') return <Analytics {...props} />
-  else if (props.type === 'emails') return <Emails {...props} />
-  else if (props.type === 'copy') return <Copy />
-  else return null
+  const icon = iconData.filter(icon => icon.name === props.type)[0] || {}
+  return <StyledIcon icon={icon} {...props} />
 }
 
 Icon.propTypes = {
