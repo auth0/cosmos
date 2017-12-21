aa6a3dcbd0d5dbbc0c36330ff4e23d978968de7e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-21 11:33:49 -0300

    delete bunch of dead code

--- tooling/docs/common/description.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Description = styled.div`
-  text-align: center;
-  padding: 10px;
-  color: #215c44;
-`
-
-export default Description
--- tooling/docs/generate.js
+++
@@ -1,5 +0,0 @@
-const getComponentMetadata = require('./metadata')
-const buildPages = require('./pages')
-
-const metadata = getComponentMetadata()
-buildPages(metadata)
