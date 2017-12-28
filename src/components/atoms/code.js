acdc763d91849b80e28c6bb4599808c3d7910c36
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-28 21:35:42 +0530

    add string parsing for code in description

--- src/components/atoms/code.js
+++ src/components/atoms/code.js
@@ -6,7 +6,7 @@ import { colors, fonts } from '../../tokens/'
 const StyledCode = styled.span`
   color: rgba(0, 0, 0, 0.86);
   font-family: ${fonts.family.code};
-  font-size: 13px;
+  font-size: 90%;
   padding: 0 6px 2px;
   display: inline-block;
   background-color: ${colors.grayLightest};
--- src/components/molecules/form/description.js
+++ src/components/molecules/form/description.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { spacing, colors } from '../../../tokens'
+import Code from '../../atoms/code'
 
 const StyledDescription = styled.div`
   font-size: 13px;
@@ -9,6 +10,33 @@ const StyledDescription = styled.div`
   margin-top: ${spacing.xsmall};
 `
 
-const Description = props => <StyledDescription>{props.children}</StyledDescription>
+const Description = props => {
+  if (typeof props.children === 'string') {
+    /*
+      if children is a string, replace instances of
+      `hash` with <Code>hash</Code>
+    */
+
+    /*
+      split the string into parts
+      example: 'use the `copy` prop'
+      becomes: ['use the ', 'copy', ' prop']
+    */
+    const parts = props.children.split('`')
+
+    /*
+      loop through and add <Code>...</Code> wrapper around
+      the correct elements
+    */
+    for (let i = 1; i < parts.length; i += 2) {
+      parts[i] = <Code>{parts[i]}</Code>
+    }
+
+    return <StyledDescription>{parts}</StyledDescription>
+  } else {
+    // if children is not a string, proxy it through
+    return <StyledDescription>{props.children}</StyledDescription>
+  }
+}
 
 export default Description
