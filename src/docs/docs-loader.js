9469d5dc9561ac854f0c0e0bfdfe6291c76d96d1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-28 18:43:23 +0530

    only show components in sidebar that have docs

--- src/docs/docs-loader.js
+++ src/docs/docs-loader.js
@@ -1,17 +1,14 @@
 /* Load examples from markdown files */
 
-const webpackMarkdownLoader = require.context(
-  '!raw-loader!./../../components/atoms/',
-  false,
-  /\.md$/
-)
+const webpackMarkdownLoader = require.context('!raw-loader!./../components', true, /\.md$/)
 
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1)
 
 const examples = webpackMarkdownLoader.keys().map(filename => {
   let name = capitalize(
     filename
-      .replace('./', '') // clean up the path
+      .split('/')
+      .pop() // take only the filename from path (/atoms/button.md)
       .replace('.md', '') // remove the extension
       .replace('_', '') // remove hint for helper components
   )
