aa3f25e8de611e7d2b41fdd0c1a0c6b1e9d429f4
Fernando Carrettoni <fcarrettoni@gmail.com>
2017-12-28 22:16:29 -0300

    Added dynamic titles to pages with Helmet

--- src/docs/home.js
+++ src/docs/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Helmet from 'react-helmet'
 
 import { Heading3, Subheader } from '../components'
 
@@ -7,9 +8,18 @@ const Container = styled.div`
   height: 100vh;
 `
 
-export default props => (
-  <Container>
-    <Heading3>Docs</Heading3>
-    <Subheader>Select a component from the sidebar</Subheader>
-  </Container>
-)
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet title="Documentation &mdash; Cosmos" />
+        <Container>
+          <Heading3>Docs</Heading3>
+          <Subheader>Select a component from the sidebar</Subheader>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Home
