45b6b3efa2d0f649f9541a850a0445d26bea87b1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-01-05 15:35:24 +0530

    remove unused imports

--- src/components/atoms/textarea.js
+++ src/components/atoms/textarea.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-import { colors, fonts, spacing, misc } from '../../tokens/'
 import { StyledInput } from './input'
 
 const StyledTextArea = StyledInput.withComponent('textarea').extend`
