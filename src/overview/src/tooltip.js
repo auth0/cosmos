8bf2df00a66e688d567cde186eba259aead80e28
Benjamin Flores <beneliflo@gmail.com>
2018-01-08 11:54:36 -0300

    added tooltip

---
+++ src/overview/src/tooltip.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Container from './container'
+import { Tooltip } from '../../components'
+
+const Tooltips = () => (
+  <Container title="Tooltip">
+    <Tooltip>I am a tooltip</Tooltip>
+  </Container>
+)
+
+export default Tooltips
