47a8aa9a241842b518e0c8386a4f4899a60a6017
Benjamin Flores <beneliflo@gmail.com>
2018-01-10 20:45:32 -0300

    now can style tab content

---
+++ src/components/molecules/tabs/content.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+import { spacing } from '../../../tokens'
+
+const StyledTabContent = styled.div``
+const TabContent = props => <StyledTabContent>{props.children}</StyledTabContent>
+
+export default TabContent
