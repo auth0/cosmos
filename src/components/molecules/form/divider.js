49719ce31514650856c2ef01e2eadd536ae58399
Fernando Carrettoni <fcarrettoni@gmail.com>
2018-01-30 19:54:40 -0300

    Removed redundant display:block to div

--- src/components/molecules/form/divider.js
+++ src/components/molecules/form/divider.js
@@ -4,7 +4,6 @@ import { fonts, spacing, colors } from '../../../tokens'
 
 const StyledDivider = styled.div`
   text-align: left;
-  display: block;
   letter-spacing: 0.5px;
   font-weight: ${fonts.weight.medium};
   border-bottom: 1px solid ${colors.form.divider};
