4dddbff33f9e477332ff2794a66e1565f168f3fb
Nate Kohari <nkohari@gmail.com>
2018-02-01 13:59:21 -0500

    Proposed new "masked" prop for TextInput to obscure hidden data without using type="password"

--- src/components/atoms/text-input/index.js
+++ src/components/atoms/text-input/index.js
@@ -7,9 +7,19 @@ const StyledInputElement = StyledInput.extend`
   height: 44px;
 `
 
-const TextInput = props => <StyledInputElement {...props} />
+const TextInput = props => {
+  if (props.masked) {
+    const length =
+      props.defaultValue && props.defaultValue.length > 0 ? props.defaultValue.length : 8
+    const maskedValue = new Array(length).join('•')
+    return <StyledInput {...props} defaultValue={maskedValue} readOnly />
+  }
+  return <StyledInput {...props} />
+}
 
 TextInput.propTypes = {
+  /** Hide input, similar to passwords but for other private information. Implies readOnly. */
+  masked: PropTypes.bool,
   /** Make input readOnly if it does not validate constraint */
   readOnly: PropTypes.bool,
   /** Use when the expected input is code */
