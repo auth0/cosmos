fb7c670d38411ebd9554b63e599a6880d74a5132
Fernando Carrettoni <fcarrettoni@gmail.com>
2018-02-06 20:30:21 -0300

    Re-added styles for separation for FormActions, FormGroups, Fieldsets and Fields

--- src/components/molecules/form/fieldset/index.js
+++ src/components/molecules/form/fieldset/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { spacing } from '../../../../tokens'
 import StyledDivider from './divider'
 
 const StyledFieldSet = styled.fieldset`
   border: none;
   padding: 0;
+  margin-bottom: ${spacing.xlarge};
+  &:last-of-type {
+    margin-bottom: 0;
+  }
 `
 
 const FieldSet = props => (
