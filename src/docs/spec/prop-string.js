b166ac94257abc4dafe483f0b614b6975c4efcff
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-02-09 10:43:45 +0530

    revert super old change

--- src/docs/spec/prop-string.js
+++ src/docs/spec/prop-string.js
@@ -1,16 +1,16 @@
 const getPropString = propData => {
-  // TODO: Refactor this block when less sleepy
-
   let propString = ''
 
+  /* filter out internal props, they start with _underscore */
   const propNames = Object.keys(propData).filter(key => key[0] !== '_')
 
   propNames.forEach(name => {
+    console.log(propData[name])
     if (propData[name].type.name === 'bool' && propData[name].value === 'true') {
       propString += ` ${name}`
     } else if (propData[name].type.name === 'string' && propData[name].value !== 'null') {
       propString += ` ${name}="${propData[name].value}"`
-    } else if (propData[name].value !== 'null') {
+    } else if (propData[name].type.name === 'number' && propData[name].value !== 'null') {
       propString += ` ${name}={${propData[name].value}}`
     }
   })
