560a0cb854768401e99dd70e7e1f28d45d63e844
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-02-09 10:24:23 +0530

    pull out defaults and switcher into their own file

---
+++ src/docs/spec/default-props.js
@@ -0,0 +1,12 @@
+const addDefaultValues = propData => {
+  /* filter out internal props, they start with _underscore */
+  const propNames = Object.keys(propData).filter(key => key[0] !== '_')
+
+  propNames.forEach(name => {
+    const { defaultValue, type } = propData[name]
+    propData[name].value = defaultValue ? defaultValue.value : 'null'
+  })
+  return propData
+}
+
+export default addDefaultValues
