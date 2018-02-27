7dd77d3fa9d492637708e176f083d5d266878906
Benjamin Flores <beneliflo@gmail.com>
2018-02-27 15:05:10 -0300

    updated avatar style

--- src/manage/client-avatar.js
+++ src/manage/client-avatar.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colors } from '../tokens'
 
 const StyledAvatar = styled.span`
   width: 100%;
   height: 100%;
-  border: 1px solid #d9d9d9;
-  border-radius: 3px;
+  background: ${colors.base.grayLightest};
+  border-radius: 50%;
   text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
 
   & svg {
-    height: 32px;
+    height: 28px;
   }
 `
 
