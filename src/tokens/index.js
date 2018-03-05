40108cd313c07d2a7150534188f621d6283cdd1a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-05 15:37:14 +0530

    move resets to cosmos/tokens

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -3,8 +3,4 @@ import spacing from './spacing'
 import fonts from './fonts'
 import misc from './misc'
 
-/* TODO: Find better place to add resets globally */
-import reset from 'auth0-cosmos/_helpers/reset'
-reset()
-
 export { colors, spacing, fonts, misc }
