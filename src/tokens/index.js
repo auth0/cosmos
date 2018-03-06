f4fa92a991739299c06d14bae29decb2eca10632
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-06 14:38:36 +0530

    add 2 themes

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -1,6 +1,12 @@
-import colors from './colors'
+import manageColors from './colors-manage'
+import extendColors from './colors-extend'
 import spacing from './spacing'
 import fonts from './fonts'
 import misc from './misc'
+
+let colors
+
+if (localStorage.cosmos_theme === 'extend') colors = extendColors
+else colors = manageColors
 
 export { colors, spacing, fonts, misc }
