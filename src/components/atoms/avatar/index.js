0f7884d2e4e8caa758a1b7b0bd94329057ab70fe
Benjamin Flores <beneliflo@gmail.com>
2018-03-12 16:43:11 -0300

    removed unused colors token

--- src/components/atoms/avatar/index.js
+++ src/components/atoms/avatar/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-import { colors } from 'auth0-cosmos-tokens'
-
 const StyledAvatar = styled.span`
   width: ${props => props.size}px;
   height: ${props => props.size}px;
