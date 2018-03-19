8f7d5f095f51acb4406fab9b1dfc523dc6d72fdd
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-19 15:09:03 +0530

    add Heading from cosmos

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Heading } from 'auth0-cosmos'
 
 const App = () => {
-  return <div>Hello world</div>
+  return <Heading size={1}>Hello world</Heading>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
