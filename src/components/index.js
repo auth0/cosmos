b16ad3891af0a73a086510df9d20ac5f8ac1ed2a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-23 00:15:27 +0530

    add some build tooling

--- src/components/index.js
+++ src/components/index.js
@@ -8,97 +8,97 @@
 /* eslint-disable import/first */
 
 /* resets for font */
-import './_helpers/globals'
+import '../build/components/_helpers/globals'
 
 /* demo */
-import Box from './atoms/_box'
+import Box from '../build/components/atoms/_box'
 export { Box }
 
 /* atoms */
-import TextInput from './atoms/text-input'
+import TextInput from '../build/components/atoms/text-input'
 export { TextInput }
 
-import Select from './atoms/select'
+import Select from '../build/components/atoms/select'
 export { Select }
 
-import Switch from './atoms/switch'
+import Switch from '../build/components/atoms/switch'
 export { Switch }
 
-import Button from './atoms/button'
+import Button from '../build/components/atoms/button'
 export { Button }
 
-import TextArea from './atoms/textarea'
+import TextArea from '../build/components/atoms/textarea'
 export { TextArea }
 
-import Logo from './atoms/logo'
+import Logo from '../build/components/atoms/logo'
 export { Logo }
 
-import Icon from './atoms/icon'
+import Icon from '../build/components/atoms/icon'
 export { Icon }
 
-import Code from './atoms/code'
+import Code from '../build/components/atoms/code'
 export { Code }
 
-import Spinner from './atoms/spinner'
+import Spinner from '../build/components/atoms/spinner'
 export { Spinner }
 
-import Tooltip from './atoms/tooltip'
+import Tooltip from '../build/components/atoms/tooltip'
 export { Tooltip }
 
-import Overlay from './atoms/overlay'
+import Overlay from '../build/components/atoms/overlay'
 export { Overlay }
 
-import Link from './atoms/link'
+import Link from '../build/components/atoms/link'
 export { Link }
 
-import Heading from './atoms/heading'
+import Heading from '../build/components/atoms/heading'
 export { Heading }
 
-import Paragraph from './atoms/paragraph'
+import Paragraph from '../build/components/atoms/paragraph'
 export { Paragraph }
 
-import Text from './atoms/text'
+import Text from '../build/components/atoms/text'
 export { Text }
 
-import Breadcrumb from './atoms/breadcrumb'
+import Breadcrumb from '../build/components/atoms/breadcrumb'
 export { Breadcrumb }
 
-import Avatar from './atoms/avatar'
+import Avatar from '../build/components/atoms/avatar'
 export { Avatar }
 
-import Thumbnail from './atoms/thumbnail'
+import Thumbnail from '../build/components/atoms/thumbnail'
 export { Thumbnail }
 
-import Image from './atoms/image'
+import Image from '../build/components/atoms/image'
 export { Image }
 
 /* molecules */
-import Dialog from './molecules/dialog'
+import Dialog from '../build/components/molecules/dialog'
 export { Dialog }
 
-import EmptyState from './molecules/empty-state'
+import EmptyState from '../build/components/molecules/empty-state'
 export { EmptyState }
 
-import Form from './molecules/form'
+import Form from '../build/components/molecules/form'
 export { Form }
 
-import FormGroup from './molecules/form-group'
+import FormGroup from '../build/components/molecules/form-group'
 export { FormGroup }
 
-import Tabs from './molecules/tabs'
+import Tabs from '../build/components/molecules/tabs'
 export { Tabs }
 
-import Stack from './molecules/stack'
+import Stack from '../build/components/molecules/stack'
 export { Stack }
 
-import List from './molecules/list'
+import List from '../build/components/molecules/list'
 export { List }
 
-import ButtonGroup from './molecules/button-group'
+import ButtonGroup from '../build/components/molecules/button-group'
 export { ButtonGroup }
 
-import PageHeader from './molecules/page-header'
+import PageHeader from '../build/components/molecules/page-header'
 export { PageHeader }
 
-import Sidebar from './molecules/sidebar'
+import Sidebar from '../build/components/molecules/sidebar'
 export { Sidebar }
