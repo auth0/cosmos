760bd21599cf687247a281bb829a9c4398359239
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-26 09:52:16 +0530

    Revert "add perf tests for usage with cosmos"
    This reverts commit 5d516b233a4c27d31334ee715ccdb45ce182b1e2.

--- examples/perf-tests/with-cosmos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Heading } from 'auth0-cosmos'
-
-const App = () => {
-  return <Heading>Hello world</Heading>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
