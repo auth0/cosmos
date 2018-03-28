76e6c32a4cf0754829d57dc933649353d1d30f22
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-28 19:41:45 +0530

    write transform for jscodeshift

---
+++ tooling/codemods/scope-usage.js
@@ -0,0 +1,10 @@
+export default function transformer(file, api) {
+  const j = api.jscodeshift
+
+  return j(file.source)
+    .find(j.ImportDeclaration)
+    .forEach(path => {
+      if (path.value.source.value === 'auth0-cosmos') path.value.source.value = '@auth0/cosmos'
+    })
+    .toSource()
+}
