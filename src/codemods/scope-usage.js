3578f2d4aff9007bb428037eb8b662e85c51785e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-03-29 14:43:05 +0530

    codemods get a package of their own

--- src/codemods/scope-usage.js
+++ src/codemods/scope-usage.js
@@ -1,4 +1,4 @@
-export default function transformer(file, api) {
+const transformer = (file, api) => {
   const j = api.jscodeshift
 
   return j(file.source)
@@ -8,3 +8,5 @@ export default function transformer(file, api) {
     })
     .toSource()
 }
+
+export default transformer
---
+++ src/components/codemods.js
@@ -0,0 +1,3 @@
+const codemods = require('auth0-cosmos-codemods')
+
+module.exports = codemods
