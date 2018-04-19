123c33e38aa83ba5a5c1c2635a7e63868cd4763d
Nate Kohari <nkohari@gmail.com>
2018-04-19 12:21:40 -0400

    Ignore null children in Tabs instead of throwing error

--- src/components/molecules/tabs/tabs.story.js
+++ src/components/molecules/tabs/tabs.story.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
+import { storiesOf, forceReRender } from '@storybook/react'
 import { Example } from '@auth0/cosmos/_helpers/story-helpers'
 
-import { Tabs } from '@auth0/cosmos'
+import { Button, Tabs } from '@auth0/cosmos'
 
 storiesOf('Tabs').add('default', () => (
   <Example title="default">
@@ -22,6 +22,16 @@ storiesOf('Tabs').add('default selected', () => (
       <Tabs.Tab selected label="Tab 3">
         Look, third tab is selected by default!
       </Tabs.Tab>
+    </Tabs>
+  </Example>
+))
+
+storiesOf('Tabs').add('null tab in children', () => (
+  <Example title="default">
+    <Tabs>
+      <Tabs.Tab label="Tab 1">This is tab 1</Tabs.Tab>
+      {null}
+      <Tabs.Tab label="Tab 2">You can render anything you want here</Tabs.Tab>
     </Tabs>
   </Example>
 ))
