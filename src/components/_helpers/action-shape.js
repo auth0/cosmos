e7a55c9188f6e58df07b4a436af6bc22e78390bb
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-05-22 15:06:54 +0530

    display expanded shape in docs

--- src/components/_helpers/action-shape.js
+++ src/components/_helpers/action-shape.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import { __ICONNAMES__ } from '@auth0/cosmos/atoms/icon'
 
+const shapeForDocs = { label: 'string', icon: 'enum', handler: 'func' }
+
 const actionShape = PropTypes.shape({
   label: PropTypes.string.isRequired,
   icon: PropTypes.oneOf(__ICONNAMES__),
@@ -13,4 +15,4 @@ const actionShapeWithRequiredIcon = PropTypes.shape({
   handler: PropTypes.func.isRequired
 })
 
-export { actionShape, actionShapeWithRequiredIcon }
+export { actionShape, actionShapeWithRequiredIcon, shapeForDocs }
