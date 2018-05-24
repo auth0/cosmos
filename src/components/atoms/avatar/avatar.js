6c2a620b2476cbcf5241998233fa2c8eb8311723
Nate Kohari <nkohari@gmail.com>
2018-05-24 13:04:30 -0400

    Add support for icons and text to Avatar

--- src/components/atoms/avatar/avatar.js
+++ src/components/atoms/avatar/avatar.js
@@ -1,72 +1,136 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { Image, Link } from '@auth0/cosmos'
+import { colors, fonts, misc, spacing } from '@auth0/cosmos-tokens'
+import Icon, { __ICONNAMES__ } from '../icon'
 
-import Image from '../image'
-
-import { misc, colors, spacing } from '@auth0/cosmos-tokens'
-
-const sizeContent = {
-  small: misc.avatar.small,
-  medium: misc.avatar.medium,
-  large: misc.avatar.large
+const iconSizes = {
+  xsmall: 14,
+  small: 16,
+  medium: 20,
+  large: 24,
+  xlarge: 36,
+  xxlarge: 52
 }
 
-const ContentAvatar = styled.div`
-  display: -webkit-box;
+const StyledAvatar = styled.span`
+  display: flex;
   align-items: center;
   justify-content: start;
 `
 
-const StyledAvatar = styled.span`
-  width: ${props => sizeContent[props.size]};
-  height: ${props => sizeContent[props.size]};
+const AvatarImage = styled.span`
+  width: ${props => misc.avatar[props.size]};
+  height: ${props => misc.avatar[props.size]};
   background-color: ${colors.base.grayLightest};
-  border-radius: 50%;
+  border: ${props => (props.shape === 'square' ? `1px solid ${colors.base.grayLight}` : 'none')};
+  border-radius: ${props => (props.shape === 'square' ? '3px' : '50%')};
+
   text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
   overflow: hidden;
-  margin-right: ${props => (props.name ? spacing.xsmall : null)};
 
   img {
     height: 100%;
     user-select: none;
   }
+
+  ${Icon.Element} {
+    line-height: 0;
+  }
+`
+
+const AvatarText = styled.div`
+  line-height: 1;
+  margin-left: ${spacing.small};
+`
+
+const AvatarTitle = styled.div`
+  color: ${colors.text.default};
+  line-height: 1;
+  font-weight: ${fonts.weight.medium};
+`
+
+const AvatarSubtitle = styled.div`
+  color: ${colors.text.secondary};
+  line-height: 1;
+  margin-top: ${spacing.xsmall};
 `
 
 const Avatar = props => {
-  if (props.name) {
-    return (
-      <ContentAvatar>
-        <StyledAvatar {...props}>
-          <Image source={props.image} />
-        </StyledAvatar>
-        <span>{props.name}</span>
-      </ContentAvatar>
+  let image
+  let text
+
+  if (props.icon) {
+    image = <Icon name={props.icon} size={iconSizes[props.size]} />
+  } else {
+    image = <Image source={props.image} />
+  }
+
+  if (props.title || props.subtitle) {
+    let title
+    let subtitle
+
+    if (props.title) {
+      if (props.href) {
+        title = (
+          <AvatarTitle>
+            <Link href={props.href}>{props.title}</Link>
+          </AvatarTitle>
+        )
+      } else {
+        title = <AvatarTitle>{props.title}</AvatarTitle>
+      }
+    }
+
+    if (props.subtitle) {
+      subtitle = <AvatarSubtitle>{props.subtitle}</AvatarSubtitle>
+    }
+
+    text = (
+      <AvatarText>
+        {title}
+        {subtitle}
+      </AvatarText>
     )
-  } else
-    return (
-      <StyledAvatar {...props}>
-        <Image source={props.image} />
-      </StyledAvatar>
-    )
+  }
+
+  const shape = props.shape || (props.icon ? 'square' : 'circle')
+
+  return (
+    <StyledAvatar>
+      <AvatarImage shape={shape} size={props.size}>
+        {image}
+      </AvatarImage>
+      {text}
+    </StyledAvatar>
+  )
 }
 
 Avatar.propTypes = {
-  /** Avatar size */
-  size: PropTypes.PropTypes.oneOf(['small', 'medium', 'large']),
-  /** Avatar image */
-  image: PropTypes.string,
-  /** User's name */
-  name: PropTypes.string
+  /** If specified, the main text will be rendered as a hyperlink with this as the target. */
+  href: PropTypes.string,
+  /* An icon to display. */
+  icon: PropTypes.oneOf(__ICONNAMES__),
+  /** An image URL to display. */
+  image: PropTypes.string.isRequired,
+  /** The shape to render the image. */
+  shape: PropTypes.oneOf(['circle', 'square']),
+  /** The size of the avatar. */
+  size: PropTypes.PropTypes.oneOf(['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge']),
+  /** The primary line of text to display. */
+  title: PropTypes.string,
+  /** The secondary line of text to display. */
+  subtitle: PropTypes.string
 }
 
 Avatar.defaultProps = {
   size: 'medium',
-  image: null,
-  name: null
+  shape: 'circle'
 }
 
 export default Avatar
+export { StyledAvatar }
--- src/components/atoms/avatar/avatar.md
+++ src/components/atoms/avatar/avatar.md
@@ -1,48 +1,106 @@
 ```meta
   category: Actions
-  description: Avatar component contains images.
+  description: Displays an image (and optionally text) for a user or resource
 ```
 
 `import { Avatar } from '@auth0/cosmos'`
 
 ```jsx
-<Avatar image='https://cdn.auth0.com/website/cosmos/cosmos-logo.svg' {props} />
+<Avatar
+  image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+  title="Auth0 Management API"
+  subtitle="System API"
+/>
 ```
 
-## Example
-
-### Avatar only with image
+`Avatar` supports several different layouts. In its most basic display, it shows an image or icon
+representing a user or a resource. It also accepts optional `title` and `subtitle` props which
+will add text to the right of the image.
 
 ```js
-<div>
+<Stack>
+  <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" />
+  <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" title="Title" />
+  <Avatar
+    image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+    title="Title"
+    subtitle="Subtitle"
+  />
+</Stack>
+```
+
+You can use the `size` prop to render `Avatars` of various sizes.
+
+```js
+<Stack>
+  <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="xsmall" />
   <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="small" />
-  <br />
   <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="medium" />
-  <br />
   <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="large" />
-</div>
+  <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="xlarge" />
+  <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" size="xxlarge" />
+</Stack>
 ```
 
-### Avatar with username
+The image or icon can also be rendered either in a circle or a square. By default, specifying an `image`
+prop will cause the `Avatar` to display a circular image, and specifying an `icon` will cause it
+to display a square image with a gray background. If necessary, you can override this default by
+specifying a value for the `shape` prop.
 
 ```js
-<div>
-  <Avatar
-    image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
-    size="small"
-    name="Cosmos DS"
-  />
-  <br />
-  <Avatar
-    image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
-    size="medium"
-    name="Cosmos DS"
-  />
-  <br />
-  <Avatar
-    image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
-    size="large"
-    name="Cosmos DS"
-  />
-</div>
+<Stack>
+  <Avatar shape="square" icon="settings" size="xsmall" />
+  <Avatar shape="square" icon="settings" size="small" />
+  <Avatar shape="square" icon="settings" size="medium" />
+  <Avatar shape="square" icon="settings" size="large" />
+  <Avatar shape="square" icon="settings" size="xlarge" />
+  <Avatar shape="square" icon="settings" size="xxlarge" />
+</Stack>
+```
+
+Here are a few more examples of `Avatars`:
+
+```js
+<Stack>
+  <div>
+    <Avatar
+      size="small"
+      image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+      title="Auth0 Management API"
+    />
+    <br />
+    <Avatar
+      size="medium"
+      image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+      title="Auth0 Management API"
+      subtitle="System API"
+    />
+    <br />
+    <Avatar
+      size="large"
+      image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+      title="Auth0 Management API"
+      subtitle="System API"
+    />
+  </div>
+  <div>
+    <Avatar size="small" shape="square" icon="settings" title="Auth0 Management API" />
+    <br />
+    <Avatar
+      size="medium"
+      shape="square"
+      icon="settings"
+      title="Auth0 Management API"
+      subtitle="System API"
+    />
+    <br />
+    <Avatar
+      size="large"
+      shape="square"
+      icon="settings"
+      title="Auth0 Management API"
+      subtitle="System API"
+    />
+  </div>
+</Stack>
 ```
