1afb5405aa34cf89b2a453299825fb8e304d1bcf
Nate Kohari <nkohari@gmail.com>
2018-05-24 15:09:56 -0400

    Fix overview and stories

--- src/overview/examples/avatars.js
+++ src/overview/examples/avatars.js
@@ -3,23 +3,75 @@ import React from 'react'
 import Example from '../ov-components/example'
 import { Avatar, Stack } from '@auth0/cosmos'
 
-const Avatars = () => (
-  <Example title="Avatar" align="center">
+const AvatarsWithImages = () => (
+  <Example title="Avatar (images)">
     <Stack>
-      <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" />
-      <Avatar image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" title="Title" />
+      <Avatar size="small" image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" />
       <Avatar
+        size="small"
+        image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+        title="Title"
+      />
+      <Avatar
+        size="small"
         image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
         title="Title"
         subtitle="Subtitle"
       />
     </Stack>
+    <br />
     <Stack>
-      <Avatar icon="settings" />
-      <Avatar icon="settings" title="Title" />
-      <Avatar icon="settings" title="Title" subtitle="Subtitle" />
+      <Avatar size="medium" image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" />
+      <Avatar
+        size="medium"
+        image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+        title="Title"
+      />
+      <Avatar
+        size="medium"
+        image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+        title="Title"
+        subtitle="Subtitle"
+      />
+    </Stack>
+    <br />
+    <Stack>
+      <Avatar size="large" image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg" />
+      <Avatar
+        size="large"
+        image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+        title="Title"
+      />
+      <Avatar
+        size="large"
+        image="https://cdn.auth0.com/website/cosmos/cosmos-logo.svg"
+        title="Title"
+        subtitle="Subtitle"
+      />
     </Stack>
   </Example>
 )
 
-export default Avatars
+const AvatarsWithIcons = () => (
+  <Example title="Avatar (icons)">
+    <Stack>
+      <Avatar size="small" icon="settings" />
+      <Avatar size="small" icon="settings" title="Title" />
+      <Avatar size="small" icon="settings" title="Title" subtitle="Subtitle" />
+    </Stack>
+    <br />
+    <Stack>
+      <Avatar size="medium" icon="settings" />
+      <Avatar size="medium" icon="settings" title="Title" />
+      <Avatar size="medium" icon="settings" title="Title" subtitle="Subtitle" />
+    </Stack>
+    <br />
+    <Stack>
+      <Avatar size="large" icon="settings" />
+      <Avatar size="large" icon="settings" title="Title" />
+      <Avatar size="large" icon="settings" title="Title" subtitle="Subtitle" />
+    </Stack>
+  </Example>
+)
+
+export { AvatarsWithImages, AvatarsWithIcons }
