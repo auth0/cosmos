c80a8df5ab402b7ef6e9c260cddfadfc530d5574
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-05-31 12:05:58 +0530

    Merge resets into globals (#572)
    Fixes https://github.com/auth0/cosmos/issues/570
    
    Super tiny side effect, the loader inside button has become smaller: https://www.chromaticqa.com/snapshot?appId=5aba7f6a07f95800202658ff&id=5b0f9717129fa30024fb6f22
    
    Can tackle that separately

--- src/components/_helpers/globals.js
+++ src/components/_helpers/globals.js
@@ -1,8 +1,61 @@
 import { injectGlobal } from 'styled-components'
-
 import { fonts, misc } from '@auth0/cosmos-tokens'
 
 injectGlobal`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+
+  /* Our resets */
+  * {
+    box-sizing: border-box;
+  }
+
+  strong, em {
+    font-weight: ${fonts.weight.bold};
+  }
 
   body, input, textarea, button, select {
     font-family: ${fonts.family.text};
@@ -70,4 +123,5 @@ injectGlobal`
     src: local('Roboto Mono'), local('RobotoMono-Regular'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
   }
+
 `
--- src/components/tokens.js
+++ src/components/tokens.js
@@ -1,7 +1,3 @@
 import { colors, spacing, fonts, misc } from '@auth0/cosmos-tokens'
 
-/* TODO: Find better place to add resets globally */
-import reset from './_helpers/reset'
-reset()
-
 export { colors, spacing, fonts, misc }
