86c66ab48a1e666413599f148818105f0ddb1c69
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-06-05 11:41:26 +0530

    remove manage specific things from overview

--- src/overview/examples/lists.js
+++ src/overview/examples/lists.js
@@ -4,7 +4,6 @@ import Section from '../ov-components/section'
 import Example from '../ov-components/example'
 
 import { List } from '@auth0/cosmos'
-import ConnectionList from '../../manage/components/connection-list'
 
 const ListsExample = () => (
   <Section>
@@ -14,9 +13,6 @@ const ListsExample = () => (
         <div>two</div>
         <div>three</div>
       </List>
-    </Example>
-    <Example title="List: Connections">
-      <ConnectionList />
     </Example>
   </Section>
 )
