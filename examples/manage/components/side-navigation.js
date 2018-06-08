d819aefbd24b5da870ea1cbb127281eef5dd5826
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-06-08 14:30:43 +0530

    copy changes nicely

--- examples/manage/components/side-navigation.js
+++ examples/manage/components/side-navigation.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
 
 import { Sidebar } from '@auth0/cosmos'
 
 class SideNavigation extends React.Component {
-  isSelected = url => this.props.location.pathname === url
+  isSelected = url => false
   render() {
     return (
       <div
@@ -18,107 +17,107 @@ class SideNavigation extends React.Component {
           <Sidebar.Link
             icon="dashboard"
             label="Dashboard"
-            url="/manage"
+            url="#/"
             selected={this.isSelected('/manage')}
           />
           <Sidebar.Link
             icon="clients"
             label="Clients"
-            url="/manage/clients"
+            url="#/clients"
             selected={this.isSelected('/manage/clients')}
           />
           <Sidebar.Link
             icon="apis"
             label="APIs"
-            url="/manage/apis"
+            url="#/apis"
             selected={this.isSelected('/manage/apis')}
           />
           <Sidebar.Link
             icon="sso-integrations"
             label="SSO Integrations"
-            url="/manage/sso-integration"
+            url="#/sso-integration"
             selected={this.isSelected('/manage/sso-integration')}
           />
           <Sidebar.LinkGroup icon="connections" label="Connections">
             <Sidebar.Link
               label="Database"
-              url="/manage/connections/database"
+              url="#/connections/database"
               selected={this.isSelected('/manage/connections/database')}
             />
             <Sidebar.Link
               label="Social"
-              url="/manage/connections/social"
+              url="#/connections/social"
               selected={this.isSelected('/manage/connections/social')}
             />
             <Sidebar.Link
               label="Enterprise"
-              url="/manage/connections/enterprise"
+              url="#/connections/enterprise"
               selected={this.isSelected('/manage/connections/enterprise')}
             />
             <Sidebar.Link
               label="Passwordless"
-              url="/manage/connections/passwordless"
+              url="#/connections/passwordless"
               selected={this.isSelected('/manage/connections/passwordless')}
             />
           </Sidebar.LinkGroup>
           <Sidebar.Link
             icon="users"
             label="Users"
-            url="/manage/users"
+            url="#/users"
             selected={this.isSelected('/manage/users')}
           />
           <Sidebar.Link
             icon="rules"
             label="Rules"
-            url="/manage/rules"
+            url="#/rules"
             selected={this.isSelected('/manage/rules')}
           />
           <Sidebar.Link
             icon="hooks"
             label="Hooks"
-            url="/manage/hooks"
+            url="#/hooks"
             selected={this.isSelected('/manage/hooks')}
           />
           <Sidebar.Link
             icon="multifactor"
             label="Multifactor Auth"
-            url="/manage/guardian"
+            url="#/guardian"
             selected={this.isSelected('/manage/guardian')}
           />
           <Sidebar.Link
             icon="hosted-pages"
             label="Hosted Pages"
-            url="/manage/login_page"
+            url="#/login_page"
             selected={this.isSelected('/manage/login_page')}
           />
           <Sidebar.LinkGroup icon="emails" label="Emails">
             <Sidebar.Link
               label="Templates"
-              url="/manage/emails"
+              url="#/emails"
               selected={this.isSelected('/manage/emails')}
             />
             <Sidebar.Link
               label="Provider"
-              url="/manage/emails/provider"
+              url="#/emails/provider"
               selected={this.isSelected('/manage/emails/provider')}
             />
           </Sidebar.LinkGroup>
           <Sidebar.Link
             icon="logs"
             label="Logs"
-            url="/manage/logs"
+            url="#/logs"
             selected={this.isSelected('/manage/logs')}
           />
           <Sidebar.Link
             icon="anomaly-detection"
             label="Anomaly Detection"
-            url="/manage/anomaly"
+            url="#/anomaly"
             selected={this.isSelected('/manage/anomaly')}
           />
           <Sidebar.Link
             icon="extensions"
             label="Extensions"
-            url="/manage/extensions"
+            url="#/extensions"
             selected={this.isSelected('/manage/extensions')}
           />
         </Sidebar>
@@ -127,4 +126,4 @@ class SideNavigation extends React.Component {
   }
 }
 
-export default withRouter(SideNavigation)
+export default SideNavigation
