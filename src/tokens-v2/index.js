22e16675fa056fe6e883cad9f95c8684702060f8
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-06-11 23:49:36 +0530

    setup basics

---
+++ src/tokens-v2/index.js
@@ -0,0 +1,3 @@
+import colors from './decisions/colors'
+
+export { colors }
---
+++ src/tokens-v2/variables/colors.js
@@ -0,0 +1,35 @@
+const variables = {
+  white: {
+    base: '#FFF' // can we document tokens like this?
+  },
+  black: {
+    light: '#333', // pull out docs from comments
+    base: '#000' // nice colocation?
+  },
+  grey: {
+    darker: '#676767',
+    dark: '#636363',
+    base: '#979797',
+    light: '#D9D9D9',
+    lighter: '#F1F1F1'
+  },
+  blue: {
+    dark: '#0a84ae',
+    base: '#11A7D9',
+    light: '#13B7ED',
+    lighter: '#44c7f4'
+  },
+  orange: {
+    base: '#eb5424',
+    light: '#ff784d',
+    lighter: '#ffb299'
+  },
+  green: {
+    base: '#4CD964'
+  },
+  red: {
+    base: '#FF0000'
+  }
+}
+
+export default variables
