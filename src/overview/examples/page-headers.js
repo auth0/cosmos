062913f1bde5ee270042467fbbab4a51eb22c43e
Nate Kohari <nkohari@gmail.com>
2018-06-21 15:50:18 -0400

    Fix overview page which is referencing component only used in the manage example app

--- src/overview/examples/page-headers.js
+++ src/overview/examples/page-headers.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 import Section from '../ov-components/section'
 import Example from '../ov-components/example'
-import { Avatar, PageHeader } from '@auth0/cosmos'
-
-import ClientTypeImages from '../../manage/components/client-types-images'
-import ClientPageHeader from '../../manage/components/client-page-header'
+import { PageHeader } from '@auth0/cosmos'
 
 const PageHeaders = () => (
   <Section>
@@ -25,20 +22,6 @@ const PageHeaders = () => (
           label: 'Tutorial',
           icon: 'play-circle',
           handler: () => {}
-        }}
-      />
-    </Example>
-    <Example title="Client Page Header">
-      <ClientPageHeader
-        title="API Explorer Client"
-        type={{
-          name: 'Non Interactive',
-          clientId: 'DUq0xuJZAD7RvezvqCrA6hpJVb6iDUip'
-        }}
-        logo={<Avatar size="large" image={ClientTypeImages.non_interactive} />}
-        breadcrumb={{
-          content: 'Clients',
-          link: '/manage/clients'
         }}
       />
     </Example>
