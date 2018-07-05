7712ba3d1a3f155e8733c79674f855869f3bd7a7
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-05 20:55:10 +0530

    remove clear for now

--- core/engine/index.js
+++ core/engine/index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-const clear = require('clear')
 const argv = require('yargs').argv
 const command = argv._[0]
-
-clear()
 
 if (command === 'start') require('./start')
 else if (command === 'build') require('./build')
