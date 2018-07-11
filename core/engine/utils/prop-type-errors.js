8ea6f27d0dc904e105f3a1975db095eaa642d9e1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-11 17:56:03 +0530

    add error overlay

---
+++ core/engine/utils/prop-type-errors.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+  /* Back up regular error */
+  const oldError = console.error
+
+  console.error = err => {
+    /* If it's a prop warning, throw the error to fail loudly */
+    if (err.includes('Failed prop type')) {
+      setTimeout(_ => {
+        throw Error(err)
+      })
+    } else {
+      /* Call regular error */
+      oldError(err)
+    }
+  }
+}
