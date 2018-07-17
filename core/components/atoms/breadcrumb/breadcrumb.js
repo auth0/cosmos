02614fbba2aaec42043cfe5e797c422d281dbe3f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-17 18:15:44 +0530

    add layers to breadcrumbs

--- core/components/atoms/breadcrumb/breadcrumb.js
+++ core/components/atoms/breadcrumb/breadcrumb.js
@@ -5,31 +5,51 @@ import PropTypes from 'prop-types'
 import { spacing, colors } from '@auth0/cosmos-tokens'
 
 import Icon from '../icon'
+import Link, { StyledLink } from '../link'
 
-const StyledBreadcrumb = styled.a`
-  color: ${colors.base.grayDark};
-  font-size: 13px;
-  text-decoration: none;
-  display: inline-block;
-  margin-bottom: ${spacing.small};
+const Wrapper = styled.div`
+  ${/* overrides for link */ ''};
 
-  ${Icon.Element} {
-    bottom: 1px;
-    position: relative;
+  ${StyledLink} {
+    font-size: 13px;
+    margin-bottom: ${spacing.small};
+  }
+
+  ${/* code for separator: */ ''};
+
+  ${StyledLink} {
+    padding: 0 ${spacing.xsmall};
+    &:after {
+      content: '/';
+      padding-left: ${spacing.xsmall};
+      color: ${colors.base.grayDark};
+    }
+  }
+
+  ${StyledLink}:last-child {
+    &:after {
+      content: '';
+    }
   }
 `
 
 const Breadcrumb = ({ link, content, ...props }) => {
   return (
-    <StyledBreadcrumb href={link || ''} {...props}>
-      <Icon name="arrow-left" size={15} color="grayDark" /> {content}
-    </StyledBreadcrumb>
+    <Wrapper>
+      <Icon name="arrow-left" size={15} color="grayDark" />
+      {props.children}
+    </Wrapper>
   )
 }
 
 Breadcrumb.propTypes = {
-  link: PropTypes.string,
-  content: PropTypes.string.isRequired
+  children: PropTypes.node
+}
+
+Breadcrumb.Link = props => <Link {...props} />
+
+Breadcrumb.Link.propTypes = {
+  link: PropTypes.string
 }
 
 export default Breadcrumb
---
+++ core/components/atoms/breadcrumb/breadcrumb.md
@@ -0,0 +1,20 @@
+```meta
+  category: Navigation
+  description: Display breadcrumbs on top of the page
+```
+
+`import { Breadcrumb } from '@auth0/breadcrumb'`
+
+---
+
+## Examples
+
+`Breadcrumb` comes with `Breadcrumb.Link` which is the familiar [Link component](#/component/link)
+
+```js
+<Breadcrumb>
+  <Breadcrumb.Link href="/home">Home</Breadcrumb.Link>
+  <Breadcrumb.Link href="/parent">Parent</Breadcrumb.Link>
+  <Breadcrumb.Link href="/parent/child">Child</Breadcrumb.Link>
+</Breadcrumb>
+```
