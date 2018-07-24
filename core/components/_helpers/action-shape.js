9c7e98ba899c1eef2cd4adb42cea24ce26f93590
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-24 14:45:17 +0530

    add props to action shape

--- core/components/_helpers/action-shape.js
+++ core/components/_helpers/action-shape.js
@@ -6,13 +6,15 @@ const shapeForDocs = { label: 'string', icon: 'enum', handler: 'func' }
 const actionShape = PropTypes.shape({
   label: PropTypes.string.isRequired,
   icon: PropTypes.oneOf(__ICONNAMES__),
-  handler: PropTypes.func.isRequired
+  handler: PropTypes.func.isRequired,
+  props: PropTypes.object
 })
 
 const actionShapeWithRequiredIcon = PropTypes.shape({
   label: PropTypes.string.isRequired,
   icon: PropTypes.oneOf(__ICONNAMES__).isRequired,
-  handler: PropTypes.func.isRequired
+  handler: PropTypes.func.isRequired,
+  props: PropTypes.object
 })
 
 export { actionShape, actionShapeWithRequiredIcon, shapeForDocs }
