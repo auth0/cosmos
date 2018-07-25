0cfc3341cbca45f15df7bbb384cfe0c4c5a85c8c
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-25 16:32:50 +0530

    use tokens from package

--- core/components/atoms/tag/tag.js
+++ core/components/atoms/tag/tag.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Icon from '../icon'
-import { spacing, fonts, colors, misc } from '../../tokens'
+import { spacing, fonts, colors, misc } from '@auth0/cosmos-tokens'
 
 const StyledTag = styled.span`
   display: inline-block;
