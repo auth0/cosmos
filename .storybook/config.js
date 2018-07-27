22adc957ed25faffc2901fe03e84f00f78646462
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-27 12:21:22 +0530

    inconsequential change

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react'
 import { getStorybook } from '@storybook/react'
 import 'react-chromatic/storybook-addon'
 
+/* This line loads all the .story files from the components directory */
 let req = require.context('../core/components/', true, /story\.js$/)
 if (process.env.SKETCH) req = require.context('../core/components/', true, /sketch\.js$/)
 
