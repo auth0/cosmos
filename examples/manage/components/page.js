0ba1e574b9e32d31426ed4644c0c4e81da85d7da
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-27 13:08:09 +0530

    cant wait for componentDidMount for tests

--- examples/manage/components/page.js
+++ examples/manage/components/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route, location } from 'react-router-dom'
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
 
 import SideNavigation from './side-navigation'
 import WelcomeIndex from '../pages/welcome'
@@ -13,39 +13,35 @@ import LogsIndex from '../pages/slogs'
 import ApisIndex from '../pages/apis'
 import UsersIndex from '../pages/users'
 
-class Page extends React.Component {
-  componentDidMount() {
-    if (window.location.hash === '#/' && this.props.defaultRoute) {
-      window.location.hash = this.props.defaultRoute
-    }
+const Page = props => {
+  if (window.location.hash === '#/' && props.defaultRoute) {
+    return <Redirect to={props.defaultRoute} />
   }
-  render() {
-    return (
-      <Router>
-        <React.Fragment>
-          <SideNavigation />
-          <div
-            style={{
-              width: '836px',
-              marginLeft: '198px',
-              paddingBottom: '50px'
-            }}
-          >
-            <Route exact path="/welcome" component={WelcomeIndex} />
-            <Route exact path="/applications" component={ApplicationIndex} />
-            <Route exact path="/applications/:applicationId" component={ApplicationDetail} />
-            <Route exact path="/emails" component={EmailsIndex} />
-            <Route exact path="/rules" component={RulesIndex} />
-            <Route exact path="/logs" component={LogsIndex} />
-            <Route exact path="/users" component={UsersIndex} />
-            <Route exact path="/apis" component={ApisIndex} />
-            <Route exact path="/anomaly" component={AnomalyDetectionIndex} />
-            <Route exact path="/sso-integrations" component={SsoIndex} />
-          </div>
-        </React.Fragment>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <React.Fragment>
+        <SideNavigation />
+        <div
+          style={{
+            width: '836px',
+            marginLeft: '198px',
+            paddingBottom: '50px'
+          }}
+        >
+          <Route exact path="/welcome" component={WelcomeIndex} />
+          <Route exact path="/applications" component={ApplicationIndex} />
+          <Route exact path="/applications/:applicationId" component={ApplicationDetail} />
+          <Route exact path="/emails" component={EmailsIndex} />
+          <Route exact path="/rules" component={RulesIndex} />
+          <Route exact path="/logs" component={LogsIndex} />
+          <Route exact path="/users" component={UsersIndex} />
+          <Route exact path="/apis" component={ApisIndex} />
+          <Route exact path="/anomaly" component={AnomalyDetectionIndex} />
+          <Route exact path="/sso-integrations" component={SsoIndex} />
+        </div>
+      </React.Fragment>
+    </Router>
+  )
 }
 
 export default Page
