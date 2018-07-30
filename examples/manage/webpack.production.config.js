f58dc919323833f8036d3f6e11692d589e717c76
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-07-30 15:18:08 +0530

    Revert "testing 1 2 3"
    This reverts commit 16252c0e57758c25480809504de90bb89d081dbd.

--- examples/manage/webpack.production.config.js
+++ examples/manage/webpack.production.config.js
@@ -5,10 +5,10 @@ const { getConfig } = require('@auth0/cosmos-engine')
 let config = getConfig('production')
 
 /* Pass a hint to cosmos to disable resets */
-// config.plugins.push(
-//   new webpack.EnvironmentPlugin({
-//     COSMOS_DISABLE_RESETS: true
-//   })
-// )
+config.plugins.push(
+  new webpack.EnvironmentPlugin({
+    COSMOS_DISABLE_RESETS: true
+  })
+)
 
 module.exports = config
