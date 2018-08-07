d38eee846f2af094d0fa77de57d8177fbc2a632b
Javier Centurion <javier.centurion@auth0.com>
2018-08-07 19:19:40 -0300

    Adding stressed story for components

--- core/components/molecules/list/list.story.js
+++ core/components/molecules/list/list.story.js
@@ -30,3 +30,13 @@ storiesOf('List').add('with stack', () => (
     </List>
   </Example>
 ))
+
+storiesOf('List').add('stressed', () => (
+  <Example title="stressed - 739 characters per row">
+    <List label="Short List">
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam sollicitudin pellentesque. Ut magna ex, ultricies in risus eget, laoreet lacinia nunc. Suspendisse mi leo, facilisis auctor massa non, congue interdum nunc. Donec eget sem id odio accumsan posuere ut ac neque. Etiam nec varius elit. Vivamus pharetra vulputate neque ut dignissim. Etiam ut malesuada nisl. Proin eget nulla venenatis ligula fringilla sodales sed in enim. Etiam luctus sollicitudin mollis. Duis a varius felis, quis egestas quam. Suspendisse sollicitudin varius condimentum. Nam accumsan placerat diam nec pretium. Vestibulum sed accumsan tellus. Nullam dictum lorem vel orci porta scelerisque. Nulla malesuada nibh ante, id dapibus felis fermentum eget.</div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam sollicitudin pellentesque. Ut magna ex, ultricies in risus eget, laoreet lacinia nunc. Suspendisse mi leo, facilisis auctor massa non, congue interdum nunc. Donec eget sem id odio accumsan posuere ut ac neque. Etiam nec varius elit. Vivamus pharetra vulputate neque ut dignissim. Etiam ut malesuada nisl. Proin eget nulla venenatis ligula fringilla sodales sed in enim. Etiam luctus sollicitudin mollis. Duis a varius felis, quis egestas quam. Suspendisse sollicitudin varius condimentum. Nam accumsan placerat diam nec pretium. Vestibulum sed accumsan tellus. Nullam dictum lorem vel orci porta scelerisque. Nulla malesuada nibh ante, id dapibus felis fermentum eget.</div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam sollicitudin pellentesque. Ut magna ex, ultricies in risus eget, laoreet lacinia nunc. Suspendisse mi leo, facilisis auctor massa non, congue interdum nunc. Donec eget sem id odio accumsan posuere ut ac neque. Etiam nec varius elit. Vivamus pharetra vulputate neque ut dignissim. Etiam ut malesuada nisl. Proin eget nulla venenatis ligula fringilla sodales sed in enim. Etiam luctus sollicitudin mollis. Duis a varius felis, quis egestas quam. Suspendisse sollicitudin varius condimentum. Nam accumsan placerat diam nec pretium. Vestibulum sed accumsan tellus. Nullam dictum lorem vel orci porta scelerisque. Nulla malesuada nibh ante, id dapibus felis fermentum eget.</div>
+    </List>
+  </Example>
+))
