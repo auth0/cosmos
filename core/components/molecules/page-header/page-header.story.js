1ce2ce578783dcb6d1a5e11144d4de268ea1c6e8
Franco Correa <franco@francocorrea.com>
2018-08-09 14:47:46 -0300

    Update page header stories to use description as children

--- core/components/molecules/page-header/page-header.story.js
+++ core/components/molecules/page-header/page-header.story.js
@@ -26,6 +26,30 @@ storiesOf('Page Header').add('default', () => (
   </Example>
 ))
 
+storiesOf('Page Header').add('default with description as children', () => (
+  <Example title="default">
+    <PageHeader
+      title="Clients"
+      description={{
+        text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
+        learnMore: '/clients'
+      }}
+      primaryAction={{
+        label: 'Create Client',
+        icon: 'plus',
+        handler: () => {}
+      }}
+      secondaryAction={{
+        label: 'Tutorial',
+        icon: 'play-circle',
+        handler: () => {}
+      }}
+    >
+      Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
+    </PageHeader>
+  </Example>
+))
+
 storiesOf('Page Header').add('only primary action', () => (
   <Example title="default">
     <PageHeader
@@ -57,5 +81,39 @@ storiesOf('Page Header').add('only secondary action', () => (
         handler: () => {}
       }}
     />
+  </Example>
+))
+
+storiesOf('Page Header').add('only primary action with description as children', () => (
+  <Example title="default">
+    <PageHeader
+      title="Clients"
+      primaryAction={{
+        label: 'Create Client',
+        icon: 'plus',
+        handler: () => {}
+      }}
+    >
+      Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
+    </PageHeader>
+  </Example>
+))
+
+storiesOf('Page Header').add('only secondary action with description as children', () => (
+  <Example title="default">
+    <PageHeader
+      title="Clients"
+      description={{
+        text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
+        learnMore: '/clients'
+      }}
+      secondaryAction={{
+        label: 'Tutorial',
+        icon: 'play-circle',
+        handler: () => {}
+      }}
+    >
+      Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
+    </PageHeader>
   </Example>
 ))
