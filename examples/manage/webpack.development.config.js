b3e673774e96d1c9bbdbb019d102544c28d2ecfb
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-08-14 15:09:31 +0530

    disable global resets for manage

--- examples/manage/webpack.development.config.js
+++ examples/manage/webpack.development.config.js
@@ -7,7 +7,7 @@ let config = getConfig('development')
 /* Pass a hint to cosmos to disable resets */
 config.plugins.push(
   new webpack.EnvironmentPlugin({
-    // COSMOS_DISABLE_RESETS: true
+    COSMOS_DISABLE_RESETS: true
   })
 )
 
