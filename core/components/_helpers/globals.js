e4bf03b4af63aba88195cb558f08a7af8fed8b01
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-08-15 16:46:05 +0530

    Add some styles despite DISABLE_GLOBAL

--- core/components/_helpers/globals.js
+++ core/components/_helpers/globals.js
@@ -132,4 +132,50 @@ if (includeGlobals) {
   }
 
 `
+} else {
+  /* We still insert some styles to keep things sane 😅 */
+  injectGlobal`
+    * {
+      box-sizing: border-box;
+    }
+    strong,
+    em {
+      font-weight: 700;
+    }
+
+    input,
+    textarea,
+    button,
+    select {
+      font-family: 'fakt-web';
+      font-weight: 400;
+      font-size: 14px;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+    /* Cosmos globals */
+
+    [class^="sc-"] {
+      line-height: 1.6;
+    }
+
+    @font-face {
+      font-family: fakt-web;
+      src: url(https://cdn.auth0.com/styleguide/core/2.0.4/fonts/fakt/FaktPro-Medium.woff2)
+          format('woff2'),
+        url(https://cdn.auth0.com/styleguide/core/2.0.4/fonts/fakt/FaktPro-Medium.woff) format('woff'),
+        url(https://cdn.auth0.com/styleguide/core/2.0.4/fonts/fakt/FaktPro-Medium.ttf)
+          format('truetype');
+      font-weight: ${fonts.weight.medium};
+      font-style: normal;
+    }
+
+    @font-face {
+      font-family: 'Roboto Mono';
+      font-style: normal;
+      font-weight: ${fonts.weight.semibold};
+      src: local('Roboto Mono'), local('RobotoMono-Regular'), url(https://fonts.gstatic.com/s/robotomono/v5/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2) format('woff2');
+      unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+    }
+  `
 }
