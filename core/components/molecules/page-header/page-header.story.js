25ebbbe12287588311ec065d1fc15fe41d333557
Franco Correa <franco@francocorrea.com>
2018-08-16 10:44:27 -0300

    Update Page header stories

--- core/components/molecules/page-header/page-header.story.js
+++ core/components/molecules/page-header/page-header.story.js
@@ -31,10 +31,11 @@ storiesOf('Page Header').add('only primary action', () => (
   <Example title="default">
     <PageHeader
       title="Clients"
-      description={{
-        text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
-        learnMore: '/clients'
-      }}
+      description={
+        <span>
+          Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
+        </span>
+      }
       primaryAction={{
         label: 'Create Client',
         icon: 'plus',
@@ -48,6 +49,41 @@ storiesOf('Page Header').add('only secondary action', () => (
   <Example title="default">
     <PageHeader
       title="Clients"
+      description={
+        <span>
+          Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
+        </span>
+      }
+      secondaryAction={{
+        label: 'Tutorial',
+        icon: 'play-circle',
+        handler: () => {}
+      }}
+    />
+  </Example>
+))
+
+storiesOf('Page Header').add('only primary action with deprecated API', () => (
+  <Example title="default">
+    <PageHeader
+      title="Clients"
+      description={{
+        text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
+        learnMore: '/clients'
+      }}
+      primaryAction={{
+        label: 'Create Client',
+        icon: 'plus',
+        handler: () => {}
+      }}
+    />
+  </Example>
+))
+
+storiesOf('Page Header').add('only secondary action with deprecated API', () => (
+  <Example title="default">
+    <PageHeader
+      title="Clients"
       description={{
         text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
         learnMore: '/clients'
@@ -58,39 +94,5 @@ storiesOf('Page Header').add('only secondary action', () => (
         handler: () => {}
       }}
     />
-  </Example>
-))
-
-storiesOf('Page Header').add('only primary action with description as children', () => (
-  <Example title="default">
-    <PageHeader
-      title="Clients"
-      primaryAction={{
-        label: 'Create Client',
-        icon: 'plus',
-        handler: () => {}
-      }}
-    >
-      Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
-    </PageHeader>
-  </Example>
-))
-
-storiesOf('Page Header').add('only secondary action with description as children', () => (
-  <Example title="default">
-    <PageHeader
-      title="Clients"
-      description={{
-        text: 'Setup a mobile, web or IoT application to use Auth0 for Authentication.',
-        learnMore: '/clients'
-      }}
-      secondaryAction={{
-        label: 'Tutorial',
-        icon: 'play-circle',
-        handler: () => {}
-      }}
-    >
-      Setup a mobile, web or IoT application to <strong>use Auth0 for Authentication</strong>.
-    </PageHeader>
   </Example>
 ))
