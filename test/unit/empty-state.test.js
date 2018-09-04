1295cd2e1901981fdc2a06e4247964d8717b43e8
Franco Correa <franco@francocorrea.com>
2018-09-04 14:59:21 -0300

    Use snapshots for render tests

--- test/unit/empty-state.test.js
+++ test/unit/empty-state.test.js
@@ -7,41 +7,23 @@ const emptyStateFactory = ({
   text = 'Some description',
   icon = 'copy',
   helpUrl = '/some-place',
-  action = { title: 'Something', icon: 'copy', handler: () => {} }
-}) => <EmptyState title={title} text={text} icon={icon} action={action} helpUrl={helpUrl} />
+  action = { label: 'Something', icon: 'copy', handler: () => {} }
+} = {}) => <EmptyState title={title} text={text} icon={icon} action={action} helpUrl={helpUrl} />
 
 describe('Empty State', () => {
-  it('renders the provided icon', () => {
-    const iconName = 'copy'
-    const wrapper = shallow(emptyStateFactory({ icon: iconName }))
-
-    const icon = wrapper.find(Icon)
-
-    expect(icon.prop('name')).toEqual(iconName)
+  it('renders properly', () => {
+    const wrapper = shallow(emptyStateFactory())
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('renders the provided title', () => {
-    const titleText = 'This is a sample title'
-    const wrapper = mount(emptyStateFactory({ title: titleText }))
-
-    const title = wrapper.find('h1')
-
-    expect(title.prop('children')).toEqual(titleText)
+  it('renders properly without help link', () => {
+    const wrapper = shallow(emptyStateFactory({ helpUrl: null }))
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('renders the help text link if present', () => {
-    const helpTextLink = '/some-place'
-    const wrapper = mount(emptyStateFactory({ helpUrl: helpTextLink }))
-
-    const link = wrapper.find('a')
-
-    expect(link.prop('href')).toEqual(helpTextLink)
-  })
-
-  it('does not render help link if not provided', () => {
-    const wrapper = mount(emptyStateFactory({ helpUrl: null }))
-
-    expect(wrapper.exists('a')).toBe(false)
+  it('renders properly without text', () => {
+    const wrapper = shallow(emptyStateFactory({ text: null }))
+    expect(wrapper).toMatchSnapshot()
   })
 
   it('renders the provided action and the helper is being called', () => {
