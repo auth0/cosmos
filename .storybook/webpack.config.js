56d7e145885ee655481098c1bc61a689745ea26b
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-09-24 16:56:49 -0300

    add styleguide to storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,11 +3,11 @@ const webpack = require('webpack')
 module.exports = storybookBaseConfig => {
   const plugins = storybookBaseConfig.plugins
 
-  // plugins.push(
-  //   new webpack.DefinePlugin({
-  //     'process.env': { SKETCH: process.env.SKETCH }
-  //   })
-  // )
+  plugins.push(
+    new webpack.EnvironmentPlugin({
+      COSMOS_DISABLE_RESETS: true
+    })
+  )
   const newConfig = { ...storybookBaseConfig }
 
   // Export bundles as libraries so we can access them on page scope.
