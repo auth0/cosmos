6813a4c97f4219fb0f5f47ae649d44781f552ab6
Franco Correa <franco@francocorrea.com>
2018-09-25 12:04:10 -0300

    New spinner variants

--- core/components/atoms/spinner/spinner.md
+++ core/components/atoms/spinner/spinner.md
@@ -9,16 +9,22 @@ desciption: Spinner is useful to notify the user of background activity
 
 ### Default spinner:
 
-```js
-<div style={{ padding: '20px' }}>
-  <Spinner />
-</div>
+```jsx
+<Spinner {props} />
 ```
 
 ### Invert for dark background
 
 ```js
 <div style={{ background: '#333', padding: '20px' }}>
-  <Spinner inverse />
+	<div style={{ marginTop: 10 }}>
+		<Spinner size="small" inverse />
+	</div>
+	<div style={{ marginTop: 10 }}>
+		<Spinner inverse />
+	</div>
+	<div style={{ marginTop: 10 }}>
+		<Spinner size="large" inverse />
+	</div>
 </div>
 ```
--- core/components/atoms/spinner/spinner.story.js
+++ core/components/atoms/spinner/spinner.story.js
@@ -5,13 +5,29 @@ import { Example, Stack } from '@auth0/cosmos/_helpers/story-helpers'
 import { Spinner } from '@auth0/cosmos'
 
 storiesOf('Spinner').add('default', () => (
-  <Example title="Spinner">
-    <Spinner />
-  </Example>
+	<React.Fragment>
+		<Example title="Spinner – small">
+			<Spinner size="small" />
+		</Example>
+		<Example title="Spinner – default">
+			<Spinner />
+		</Example>
+		<Example title="Spinner – large">
+			<Spinner size="large" />
+		</Example>
+	</React.Fragment>
 ))
 
 storiesOf('Spinner').add('dark background', () => (
-  <Example title="Spinner" background="dark">
-    <Spinner inverse />
-  </Example>
+	<React.Fragment>
+		<Example title="Spinner – small" background="dark">
+			<Spinner inverse size="small" />
+		</Example>
+		<Example title="Spinner – default" background="dark">
+			<Spinner inverse />
+		</Example>
+		<Example title="Spinner – large" background="dark">
+			<Spinner inverse size="large" />
+		</Example>
+	</React.Fragment>
 ))
