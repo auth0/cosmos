6aa215ff80184f45a557296b16b6340e86c61e32
Franco Correa <franco.correa@auth0.com>
2018-09-26 18:16:44 -0300

    Add named icons story (#943)

--- core/components/_helpers/story-helpers.js
+++ core/components/_helpers/story-helpers.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Example from './story-example'
 import Stack from './story-stack'
 
-export { Example, Stack }
+const IconStoryBox = styled.div`
+	width: 15%;
+	display: inline-block;
+	text-align: center;
+	padding: 10px;
+`
+
+export { Example, Stack, IconStoryBox }
