0f5cbc1e794f588550e09c5fb841586257941ebf
Fernando Carrettoni <fcarrettoni@gmail.com>
2018-09-28 19:46:55 -0300

    More prominent version switcher for docs

--- internal/docs/docs-components/version-switcher.js
+++ internal/docs/docs-components/version-switcher.js
@@ -12,38 +12,26 @@ let versions = lines.map(line => line.split('## ')[1].split(' [')[0])
 /* remove versions older than 0.5.1 */
 versions = versions.filter(version => version > '0.5.0')
 
-const Wrapper = styled.span`
-  font-size: 14px;
-  letter-spacing: 1.4px;
-  1display: inline-block;
-  color: ${colors.base.grayLightest};
-  font-weight: 700;
-  margin-left: 16px;
-
-  select {
-    background: transparent;
-    border: none;
-    font-size: 12px;
-    color: ${colors.base.grayLight};
-  }
+const StyledSelect = styled.select`
+  background: transparent;
+  border: 1px solid #8e8e8e;
+  font-size: 13px;
+  color: ${colors.base.grayLight};
 `
 
 const VersionSwitcher = () => (
-  <Wrapper>
-    COSMOS
-    <select
-      onChange={event => {
-        const version = event.target.value.replace(/\./g, '-')
-        window.location.href = `https://auth0-cosmos-${version}.now.sh`
-      }}
-    >
-      {versions.map(v => (
-        <option key={v} value={v}>
-          {v}
-        </option>
-      ))}
-    </select>
-  </Wrapper>
+  <StyledSelect
+    onChange={event => {
+      const version = event.target.value.replace(/\./g, '-')
+      window.location.href = `https://auth0-cosmos-${version}.now.sh`
+    }}
+  >
+    {versions.map(v => (
+      <option key={v} value={v}>
+        {v}
+      </option>
+    ))}
+  </StyledSelect>
 )
 
 export default VersionSwitcher
