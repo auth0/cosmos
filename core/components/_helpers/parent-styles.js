14a084d7b20aff7a6e4baa11d95e46668d22fb90
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-09-28 12:04:19 -0300

    create parent styles helper

---
+++ core/components/_helpers/parent-styles.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components'
+import { misc } from '@auth0/cosmos-tokens'
+
+const parentStyles = css`
+  line-height: ${misc.lineHeight};
+`
+
+export default parentStyles
