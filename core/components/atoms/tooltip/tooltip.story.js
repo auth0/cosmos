a1b9da410c2f6f6802ca3aaf661e656c2c64cff8
Franco Correa <franco@francocorrea.com>
2018-10-01 14:14:48 -0300

    Add stories for tooltip

---
+++ core/components/atoms/tooltip/tooltip.story.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Example, Stack } from '@auth0/cosmos/_helpers/story-helpers'
+
+import { Tooltip, ButtonGroup, Button } from '@auth0/cosmos'
+
+const positions = ['top', 'left', 'right', 'bottom']
+
+storiesOf('Tooltip').add('with button', () =>
+  positions.map(position => (
+    <Example title={position}>
+      <div style={{ marginLeft: position === 'left' ? 120 : 0 }}>
+        <Tooltip position={position} content={`Inner content ${position}`} defaultVisible>
+          <Button>Something</Button>
+        </Tooltip>
+      </div>
+    </Example>
+  ))
+)
+
+storiesOf('Tooltip').add('with button group', () =>
+  positions.map(position => (
+    <Example title={position}>
+      <div style={{ marginLeft: position === 'left' ? 120 : 0 }}>
+        <ButtonGroup>
+          <Button>Something</Button>
+          <Tooltip position={position} content={`Inner content ${position}`} defaultVisible>
+            <Button>Something</Button>
+          </Tooltip>
+          <Button>Something</Button>
+        </ButtonGroup>
+      </div>
+    </Example>
+  ))
+)
