44c2a127bbe8c6ad5a24ea718f5bba12a0eb3367
Franco Correa <franco@francocorrea.com>
2018-10-02 16:56:38 -0300

    Add quotes to some env vars

--- package-scripts.js
+++ package-scripts.js
@@ -52,7 +52,7 @@ module.exports = {
       },
       chromaticci: {
         script:
-          'if [[ $TRAVIS_EVENT_TYPE != "pull_request" || $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]]; then chromatic test --storybook-addon --script-name=sandbox --exit-zero-on-changes; fi;',
+          'if [[ $TRAVIS_EVENT_TYPE != "pull_request" || "$TRAVIS_PULL_REQUEST_SLUG" != "$TRAVIS_REPO_SLUG" ]]; then chromatic test --storybook-addon --script-name=sandbox --exit-zero-on-changes; fi;',
         description: 'Check if CI event is push and then run chromatic'
       },
       unit: {
