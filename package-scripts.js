3b43a274c8fe563db1e92448b6d506134ac9d6ee
Franco Correa <franco@francocorrea.com>
2018-10-03 14:52:53 -0300

    Add external build fixes

--- package-scripts.js
+++ package-scripts.js
@@ -52,7 +52,7 @@ module.exports = {
       },
       chromaticci: {
         script:
-          'if-env TRAVIS_EVENT_TYPE=push && chromatic test --storybook-addon --script-name=sandbox --exit-zero-on-changes || echo "Skip chromatic"',
+          'if [ $TRAVIS_EVENT_TYPE != "pull_request" -o "$TRAVIS_PULL_REQUEST_SLUG" != "$TRAVIS_REPO_SLUG" ]; then chromatic test --storybook-addon --script-name=sandbox --exit-zero-on-changes; fi;',
         description: 'Check if CI event is push and then run chromatic'
       },
       snapshot: {
