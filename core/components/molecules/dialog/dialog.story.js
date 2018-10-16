1150e8c82159229eb0bee0db47e0dc6c6ea618fd
Franco Correa <franco@francocorrea.com>
2018-10-16 15:06:18 -0300

    Update snapshots

--- core/components/molecules/dialog/dialog.story.js
+++ core/components/molecules/dialog/dialog.story.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 import { Example, Stack } from '@auth0/cosmos/_helpers/story-helpers'
-import { Dialog, Form, Link, Tabs, Paragraph, Text } from '@auth0/cosmos'
+import { Dialog, Form, Link, Tabs, Paragraph, Text, Button } from '@auth0/cosmos'
 
 const StyledExample = styled(Example)`
   min-height: 800px;
@@ -39,6 +39,7 @@ storiesOf('Dialog').add('with primary button only', () => (
 
 storiesOf('Dialog').add('with secondary button only', () => (
   <StyledExample title="with secondary button only">
+    <Button>test</Button>
     <Dialog
       open
       title="Example Dialog"
@@ -47,6 +48,7 @@ storiesOf('Dialog').add('with secondary button only', () => (
     >
       Are you sure?
     </Dialog>
+    <Button>test</Button>
   </StyledExample>
 ))
 
