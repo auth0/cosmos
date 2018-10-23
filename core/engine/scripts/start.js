76021ee3ccf448d0b8d37fbc18ce5081cbb1587d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-10-23 17:39:26 +0530

    write metadata

--- core/engine/scripts/start.js
+++ core/engine/scripts/start.js
@@ -1,9 +1,28 @@
 const path = require('path')
 
-//TODO: Read relative path from config
-const relativeComponentsDir = './core/components'
+const getConfig = require('../utils/get-config')
+const getFiles = require('../utils/get-files')
+const appendComponentInfo = require('../utils/append-componentinfo.js')
+const enhanceProps = require('../utils/enhance-props.js')
+const appendDocumentation = require('../utils/append-documentation.js')
+const writeMetadata = require('../utils/write-metadata.js')
 
-const componentsDir = path.join(process.cwd(), relativeComponentsDir)
-const files = require('../utils/get-files')(componentsDir)
+const { componentsDir } = getConfig()
 
-console.log(files.componentFiles)
+/* Get all relevant files */
+const files = getFiles(componentsDir)
+
+/* Convert filePath into component object */
+const convertToComponentObject = filePath => {
+  return { filePath: filePath }
+}
+
+/* Generate component metadata: pipe down each function */
+let metadata = files.componentFiles
+  .map(convertToComponentObject) /* 1. Convert filePath into component object */
+  .map(appendComponentInfo) /* 2. Append component info using react-docgen */
+  .map(enhanceProps) /* 3. Enhance prop data to improve documentation */
+  .map(appendDocumentation) /* 4. Append documentation from md file */
+
+/* Write metadata into file */
+writeMetadata(metadata)
---
+++ core/engine/utils/enhance-props.js
@@ -0,0 +1,42 @@
+const enhanceProps = component => {
+  /* make modifications to prop types to improve documentation */
+  if (component.props) {
+    Object.values(component.props).forEach(prop => {
+      /* remove redundant quotes from default value of type string */
+      if (prop.defaultValue) {
+        if (typeof prop.defaultValue.value === 'string') {
+          prop.defaultValue.value = prop.defaultValue.value.replace(/^'(.*)'$/, '$1')
+        }
+      }
+
+      /* remove redundant quotes from enum values in prop types */
+      if (prop.type.name === 'enum' && Array.isArray(prop.type.value)) {
+        prop.type.value.forEach(element => {
+          element.value = element.value.replace(/^'(.*)'$/, '$1')
+        })
+      }
+
+      /* add required field for custom shapes */
+      if (prop.type.name === 'custom' && prop.type.raw.includes('.isRequired')) {
+        prop.required = true
+        prop.type.raw = prop.type.raw.replace('.isRequired', '')
+      }
+
+      // way too cosmos specific:
+
+      // if (prop.type.name === 'enum' && prop.type.value === '__ICONNAMES__') {
+      //   /* create an array of all the icons with an empty string as first element */
+      //   prop.type.value = [{ value: '' }].concat(Object.keys(icons).map(value => ({ value })))
+      // }
+      //
+      // if (prop.type.name === 'enum' && prop.type.value === '__COLORS__') {
+      //   /* create an array of all the base colors */
+      //   prop.type.value = Object.keys(colors.base).map(value => ({ value }))
+      // }
+    })
+  }
+
+  return component
+}
+
+module.exports = enhanceProps
