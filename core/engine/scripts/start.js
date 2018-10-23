a31beb993aaa90c297f696573b0cc9dd2d669e85
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-10-23 16:33:22 +0530

    get files

--- core/engine/scripts/start.js
+++ core/engine/scripts/start.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
-const componentsDir = path.join(process.cwd(), './core/componentsDir')
+//TODO: Read from config
+const relativeComponentsDir = './core/components'
 
+const componentsDir = path.join(process.cwd(), relativeComponentsDir)
 const files = require('../utils/get-files')(componentsDir)
 
 console.log(files)
--- core/engine/utils/get-files.js
+++ core/engine/utils/get-files.js
@@ -1,5 +1,11 @@
+const glob = require('glob')
+const path = require('path')
+
 const getFiles = componentsDir => {
-  return componentsDir
+  const javascriptFiles = glob.sync(path.join(componentsDir, '**/*.js'))
+  const markdownFiles = glob.sync(path.join(componentsDir, '**/*.md'))
+
+  return { javascriptFiles, markdownFiles }
 }
 
 module.exports = getFiles
