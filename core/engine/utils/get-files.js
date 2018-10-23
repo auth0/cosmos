d7cf517888f9bc04d8f77ea25fe6f558e48ca51c
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-10-23 16:49:35 +0530

    skip difinition files

--- core/engine/utils/get-files.js
+++ core/engine/utils/get-files.js
@@ -5,12 +5,13 @@ const patterns = {
   md: '**/*.md',
   js: '**/*.*(js|jsx|ts)',
   story: '**/*.story.*(js|jsx|ts)',
-  sketch: '**/*.sketch.*(js|jsx|ts)'
+  sketch: '**/*.sketch.*(js|jsx|ts)',
+  definitions: '**/*.d.ts'
 }
 
 const getFiles = componentsDir => {
   const componentFiles = glob.sync(path.join(componentsDir, patterns.js), {
-    ignore: [patterns.story, patterns.sketch]
+    ignore: [patterns.story, patterns.sketch, patterns.definitions]
   })
   const markdownFiles = glob.sync(path.join(componentsDir, patterns.md))
   const storyFiles = glob.sync(path.join(componentsDir, patterns.story))
