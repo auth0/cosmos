b8363ca4fec9d78c0e8a9bb7c0f31d7c375ea911
Franco Correa <franco@francocorrea.com>
2018-11-01 15:27:18 -0300

    Fix margin

--- core/components/atoms/button/button.js
+++ core/components/atoms/button/button.js
@@ -267,10 +267,10 @@ Button.Element = styled.button`
 
 Button.Text = styled.span`
   display: inline-block;
-  vertical-align: middle
-  
-  // Sub-pixel position adjustment
-  // See: https://github.com/auth0/cosmos/pull/947
+  vertical-align: middle;
+
+  /* Sub-pixel position adjustment */
+  /* See: https://github.com/auth0/cosmos/pull/947 */
   margin-top: 2px;
 `
 
