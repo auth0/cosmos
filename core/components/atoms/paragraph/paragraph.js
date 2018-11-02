bd04da99881cad9ccc7a4f5b8c651f9bfb8c4f77
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-11-02 14:31:40 +0530

    introduce margin prop

--- core/components/atoms/paragraph/paragraph.js
+++ core/components/atoms/paragraph/paragraph.js
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { colors, fonts } from '@auth0/cosmos-tokens'
+import margin from '../../_helpers/margin'
 
 const StyledParagraph = styled.p`
   margin: 1em 0;
+  ${margin}; /* overrides */
   color: ${colors.text.default};
   font-size: 14px;
   font-weight: ${fonts.weight.normal};
--- examples/manage/pages/application-detail/connections.js
+++ examples/manage/pages/application-detail/connections.js
@@ -5,21 +5,14 @@ import { Paragraph } from '@auth0/cosmos'
 
 import ConnectionList from '../../components/connection-list'
 
-// TODO: Need to figure out how to make the first Paragraph have no margin top. We could add a prop?
-// TODO: Remove and solve this override!
-const FirstParagraph = styled(Paragraph)`
-  margin-top: 0;
-`
-
 class Connections extends React.Component {
   render() {
     return (
       <div>
-        <FirstParagraph>
+        <Paragraph margin={{ top: 'large' }}>
           Connections are sources of users. They are categorized into Database, Social and
           Enterprise and can be shared among different applications.
-        </FirstParagraph>
-
+        </Paragraph>
         <ConnectionList />
       </div>
     )
