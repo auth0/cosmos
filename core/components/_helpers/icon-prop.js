026b303a48ebf05ae7c4992739c118acddc477cd
Franco Correa <franco@francocorrea.com>
2018-11-07 14:34:55 -0300

    Add additional props to icon prop helper

--- core/components/_helpers/icon-prop.js
+++ core/components/_helpers/icon-prop.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Icon from '../atoms/icon'
 
-export default prop => {
+const applyAdditionalProps = (element, props) => {
+  const propsArePresent = props && Object.keys(props).length > 0
+
+  return propsArePresent ? React.cloneElement(element, props) : element
+}
+
+export default (prop, additionalProps = {}) => {
   if (typeof prop === 'string') {
     console.warn('Passing icons as string has been deprecated and will be removed in Cosmos 1.0.')
     console.log(`Please replace 'icon="${prop}"' with 'icon={<Icon name="${prop}"/>}'`)
-    return <Icon name={prop} />
+
+    return <Icon name={prop} {...additionalProps} />
   }
-  return prop
+
+  return applyAdditionalProps(prop, additionalProps)
 }
