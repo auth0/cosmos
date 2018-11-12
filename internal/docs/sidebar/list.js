be70adf9b54791eccd09b515af2d0448d4511f6d
Andres Galante <agalante@gmail.com>
2018-11-12 13:42:53 -0300

    adds system guidelines to the docs

--- internal/docs/sidebar/list.js
+++ internal/docs/sidebar/list.js
@@ -31,6 +31,7 @@ const guides = [
   { path: '/', title: 'Getting started' },
   { path: '/usage', title: 'How to use Cosmos?' },
   { path: '/guiding-principles', title: 'Guiding Principles' },
+  { path: '/system-guidelines', title: 'System Guidelines' },
   { path: '/contribution-guide', title: 'Contributing to Cosmos' },
   { path: '/faqs', title: 'FAQs' },
   { path: '/changes', title: 'Changelog' },
