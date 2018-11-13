e91fa501517c5bfb16a2bf34e2cc15c45f3905b0
Franco Correa <franco@francocorrea.com>
2018-11-13 13:46:35 -0300

    Form Group

--- core/components/molecules/form-group/form-group.js
+++ core/components/molecules/form-group/form-group.js
@@ -6,21 +6,20 @@ import Automation from '../../_helpers/automation-attribute'
 import Well from '../../atoms/_well'
 import { spacing } from '@auth0/cosmos-tokens'
 
-const StyledFormGroup = styled.div``
+const FormGroup = props => {
+  const children = React.Children.map(props.children, child => {
+    return <FormGroup.FormWrapper>{child}</FormGroup.FormWrapper>
+  })
 
-const StyledFormWrapper = styled(Well)`
+  return <FormGroup.Element {...Automation('form-group')}>{children}</FormGroup.Element>
+}
+
+FormGroup.Element = styled.div``
+
+FormGroup.FormWrapper = styled(Well)`
   margin-bottom: ${spacing.medium};
   padding-bottom: ${spacing.xlarge};
 `
-
-const FormGroup = props => {
-  const children = React.Children.map(props.children, child => {
-    return <StyledFormWrapper>{child}</StyledFormWrapper>
-  })
-
-  return <StyledFormGroup {...Automation('form-group')}>{children}</StyledFormGroup>
-}
-
 FormGroup.propTypes = {
   /** children should be Form components */
   children: PropTypes.arrayOf(PropTypes.element)
