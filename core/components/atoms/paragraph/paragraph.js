6483aaa453cfb1ad4304b1fbf484222bc49fa6d9
Franco Correa <franco.correa@auth0.com>
2018-11-28 18:10:13 -0300

    Apply container styles to paragraph (#1165)

--- core/components/atoms/paragraph/paragraph.js
+++ core/components/atoms/paragraph/paragraph.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import containerStyles from '../../_helpers/container-styles'
 
 import { colors, fonts } from '@auth0/cosmos-tokens'
 
 const StyledParagraph = styled.p`
+  ${containerStyles};
   margin: 1em 0;
   color: ${colors.text.default};
   font-size: 14px;
