097a676544264214ba401bc04c529012d065197e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-03 14:44:56 +0530

    lets run it in watch mode

--- internal/test/chromatic.js
+++ internal/test/chromatic.js
@@ -6,7 +6,7 @@ if (!ci) {
   process.exit(1)
 }
 
-if (event === 'pull_request') {
+if (event !== 'push') {
   console.log('This script only runs on the "push" event, check the other build')
   process.exit(0)
 }
@@ -27,6 +27,7 @@ task({
   label: 'Running chromatic',
   command: 'chromatic',
   params,
+  watch: true,
   verbose: true,
   failOnError: true
 })
