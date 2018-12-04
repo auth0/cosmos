3ff15695a662f170ef52ef8028ccaa8fc268c95d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-04 17:07:39 +0530

    tiny implementation

--- core/components/styled.js
+++ core/components/styled.js
@@ -8,6 +8,9 @@ import styled, {
 
 import domElements from 'styled-components/src/utils/domElements'
 
+/* import cosmos specific helpers */
+import margin from './styled-margin'
+
 /* create a thin replacement for styled */
 let styledWithHelpers = styledComponent => {
   return styled(styledComponent)
@@ -16,7 +19,16 @@ let styledWithHelpers = styledComponent => {
 /* create functions for all the elements supported in styled */
 domElements.forEach(domElement => {
   styledWithHelpers[domElement] = (styles, ...interpolations) => {
-    /* this is where we append or prepend custom logic */
+    /*
+      this is where we can add custom interpolation logic:
+
+      defaults should be added to the start of the list
+      overrides should be added to the end of the list
+
+      you can add a function that looks like this:
+      const margin: function = (props: object) => css: string
+    */
+    interpolations.push(margin)
 
     return styled(domElement)(styles, ...interpolations)
   }
