ac160424553e5ebd09873ba00e52f2c378236c6d
andresgalante <agalante@gmail.com>
2018-12-04 15:42:45 -0300

    allows fit options on images

--- core/components/atoms/image/image.js
+++ core/components/atoms/image/image.js
@@ -2,11 +2,30 @@ import React from 'react'
 import styled from '@auth0/cosmos/styled'
 import PropTypes from 'prop-types'
 
-const Image = props => <Image.Element src={props.source} alt={props.alt} {...props} />
+const imageFit = {
+  'fill': 'fill',
+  'contain': 'contain',
+  'cover': 'cover',
+  'none': 'none',
+  'scale-down': 'scale-down'
+}
 
-Image.Element = styled.img`
-  user-select: none;
-  pointer-events: none;
+const Image = props => {
+  return (
+    <Image.Element>
+      <Image.Image src={props.source} alt={props.alt} {...props} />
+      <figcaption>An elephant at sunset</figcaption>
+    </Image.Element>
+  )
+}
+
+Image.Element = styled.figure`
+`
+
+Image.Image = styled.img`  
+  object-fit: ${props => imageFit[props.imageFit]};;
+  height: 100%;
+  width: 100%;
 `
 
 Image.propTypes = {
@@ -22,3 +41,29 @@ Image.defaultProps = {
 }
 
 export default Image
+
+
+
+// .figure {
+//   // Ensures the caption's text aligns with the image.
+//   display: inline-block;
+// }
+
+// .figure-img {
+//   margin-bottom: ($spacer / 2);
+//   line-height: 1;
+// }
+
+// .figure-caption {
+//   font-size: $figure-caption-font-size;
+//   color: $figure-caption-color;
+// }
+
+
+// fit: PropTypes.oneOf([
+//   'fill',
+//   'contain',
+//   'cover',
+//   'none',
+//   'scale-down'
+// ]),
