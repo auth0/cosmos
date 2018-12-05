d57ca89e4dc5114afdd6d1a45bc723ac03ff1927
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-05 23:58:32 +0530

    styled-components-with-helpers (#1208)
    For the API suggested in margin https://github.com/auth0/cosmos/pull/1059 and overrides https://github.com/auth0/cosmos/issues/1105, we need to add a helper/utility to all presentational components
    
    To see an example, see #1209 (Margin prop implementation)
    
    Because we use `styled-components` to create our presentational components, creating a wrapper around `styled` is a nice-ish way to maintain it.
    
    Pros: 
    1. We won't need to remember adding it in every component
    2. Slightly easier to upgrade styled-components
    
    ---
    
    I've added a story to test the use cases. All styled features work pretty nicely with one exception: `attrs` ([more info here](https://github.com/auth0/cosmos/pull/1208/files#diff-66c5a24f5b5148950fc22c810d546149R40)). Only [one component](https://github.com/auth0/cosmos/blob/f0d93df60f3a640931bced9a55ecbc77a323f5a8/core/components/molecules/danger-zone/danger-zone.js#L48) uses it, we can refactor that
    
    The real test of this PR will be when all of the components migrate to it.
    
    ---
    
    This is not the massive PR that refactors components, that will come after.

---
+++ core/components/_helpers/component.story.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { colors } from '@auth0/cosmos-tokens'
+import { Stack } from '@auth0/cosmos'
+import styled, { css } from '@auth0/cosmos/styled'
+
+const borderStyles = css`
+  border: 2px solid black;
+`
+
+const Box = styled.div`
+  width: 50px;
+  height: 50px;
+  /* understands tokens */
+  color: ${colors.base.white};
+  /* understands props */
+  background: ${props => colors.base[props.bg] || '#ccc'};
+  /* css helper works */
+  ${borderStyles};
+`
+
+/* extend works */
+const TallBox = Box.extend`
+  height: 200px;
+`
+
+/* styled wrapper works */
+const ShortBox = styled(Box)`
+  height: 20px;
+`
+
+/* withComponent works */
+const InputBox = Box.withComponent('input').extend`
+  :hover {
+    background: #fff;
+  }
+`
+
+/* attrs doesn't work properly
+  completely breaks:
+  const PasswordBox = styled.input.attrs({ type: 'password' })``
+  works without helpers
+  const PasswordBox = styled('input').attrs({ type: 'password' })``
+*/
+
+// weird workaround that works:
+const PasswordBox = styled(InputBox).attrs({ type: 'password' })``
+
+storiesOf('Component', module).add('default', () => (
+  <Stack>
+    <Box />
+    <Box bg="blue" />
+    <TallBox bg="blue" />
+    <ShortBox bg="blue" />
+    <InputBox bg="blue" defaultValue="okay" />
+    <PasswordBox bg="blue" defaultValue="okay" />
+  </Stack>
+))
