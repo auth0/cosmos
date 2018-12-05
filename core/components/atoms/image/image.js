9d23727e27c55e75883a97e166d0ed07d66ba05e
andresgalante <agalante@gmail.com>
2018-12-05 11:39:21 -0300

    add image size

--- core/components/atoms/image/image.js
+++ core/components/atoms/image/image.js
@@ -3,18 +3,22 @@ import styled from '@auth0/cosmos/styled'
 import PropTypes from 'prop-types'
 
 const imageFit = {
-  fill: 'fill',
   contain: 'contain',
   cover: 'cover',
   none: 'none',
   'scale-down': 'scale-down'
 }
 
+const imageSize = {
+  height: 'contain',
+  width: 'cover'
+}
+
 const Image = props => <Image.Element src={props.source} alt={props.alt} {...props} />
 
 Image.Element = styled.img`
   object-fit: ${props => imageFit[props.imageFit]};
-  height: 100%;
+  height: auto;
   width: 100%;
 `
 
@@ -24,7 +28,7 @@ Image.propTypes = {
   /** Alt info for the image */
   alt: PropTypes.string,
   /** Sets how the image should be resized to fit its container. */
-  imageFit: PropTypes.oneOf(['fill', 'contain', 'cover', 'none', 'scale-down'])
+  imageFit: PropTypes.oneOf(['contain', 'cover', 'none', 'scale-down'])
 }
 
 Image.defaultProps = {
