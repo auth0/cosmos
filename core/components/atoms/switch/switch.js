3d7fb8796584c3311056a125445aa0ba9cc2cfd1
Pablo Terradillos <tehsis@gmail.com>
2018-12-13 14:45:53 -0300

    Explictly declare element ordering in switch (#1274)
    I have reworked over #1273. Instead of using an array to order elements I explicitly order the elements surrounding them by fragments assigning them to the elements variable.

--- core/components/atoms/switch/switch.js
+++ core/components/atoms/switch/switch.js
@@ -28,15 +28,13 @@ class Switch extends React.Component {
   }
   render() {
     let [onLabel, offLabel] = this.props.accessibleLabels
-    let elements = [
-      <Checkbox
-        type="checkbox"
-        checked={this.state.on}
-        readOnly
-        id={this.props.id}
-        key="switch-checkbox"
-      />
-    ]
+
+    const checkbox = (
+      <Checkbox type="checkbox" checked={this.state.on} readOnly id={this.props.id} />
+    )
+
+    let elements = <React.Fragment>{checkbox}</React.Fragment>
+
     const label = (
       <Label labelPosition={this.props.labelPosition} key="switch-label">
         {this.state.on ? onLabel : offLabel}
@@ -45,11 +43,21 @@ class Switch extends React.Component {
     const toggle = <Toggle on={this.state.on} readOnly={this.props.readOnly} key="switch-toggle" />
 
     if (this.props.labelPosition == 'left') {
-      if (!this.props.hideAccessibleLabels) elements.push(label)
-      elements.push(toggle)
+      elements = (
+        <React.Fragment>
+          {checkbox}
+          {this.props.hideAccessibleLabels ? null : label}
+          {toggle}
+        </React.Fragment>
+      )
     } else if (this.props.labelPosition == 'right') {
-      elements.push(toggle)
-      if (!this.props.hideAccessibleLabels) elements.push(label)
+      elements = (
+        <React.Fragment>
+          {checkbox}
+          {toggle}
+          {this.props.hideAccessibleLabels ? null : label}
+        </React.Fragment>
+      )
     }
 
     /*
