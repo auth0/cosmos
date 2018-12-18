b22b02c413a50125f0393cfe6eb9769a8dfefc2b
andresgalante <agalante@gmail.com>
2018-12-18 13:00:41 -0300

    adds fit and size options to the image

--- core/components/atoms/image/image.js
+++ core/components/atoms/image/image.js
@@ -2,23 +2,43 @@ import React from 'react'
 import styled from '@auth0/cosmos/styled'
 import PropTypes from 'prop-types'
 
+const imageFit = {
+  fill: 'fill',
+  contain: 'contain',
+  cover: 'cover',
+  none: 'none',
+  'scale-down': 'scale-down'
+}
+
 const Image = props => <Image.Element src={props.source} alt={props.alt} {...props} />
 
 Image.Element = styled.img`
-  height: auto;
+  display: block;
+  height: ${props => props.height};
+  width: ${props => props.width};
   max-width: 100%;
+  object-fit: ${props => imageFit[props.fit]};
 `
 
 Image.propTypes = {
   /** URL of the image */
   source: PropTypes.string.isRequired,
   /** Alt text is mandatory, please read the accessibility section */
-  alt: PropTypes.string
+  alt: PropTypes.string,
+  /** Image height, messured in pixels */
+  height: PropTypes.number,
+  /** Image width, messured in pixels */
+  width: PropTypes.number,
+  /** Sets how the content of the image is been resized to fit its container. */
+  fit: PropTypes.oneOf(['none', 'fill', 'contain', 'cover', 'scale-down'])
 }
 
 Image.defaultProps = {
   source: null,
-  alt: null
+  alt: null,
+  width: null,
+  height: null,
+  fit: 'none'
 }
 
 export default Image
