808b946221f21c7a1423b7ce4b5c2e899d412d61
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-19 16:31:51 +0530

    textarea

---
+++ internal/test/integration/textarea.fixture.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextArea } from '@auth0/cosmos'
+
+class Fixture extends React.Component {
+  render() {
+    return <TextArea id="custom-id" placeholder="Placeholder text" />
+  }
+}
+
+export default Fixture
