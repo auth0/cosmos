ad7e74970406beb5f5acce293d9dc2043f1a09d2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-19 16:02:37 +0530

    image

---
+++ internal/test/integration/image.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import Fixture from './image.fixture'
+import customIdTest from './helpers/custom-id'
+
+test('Accepts custom id prop', () => {
+  return customIdTest(Fixture, 'image')
+})
