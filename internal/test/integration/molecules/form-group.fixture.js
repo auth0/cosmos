62f6499ab620ec20d0591c2318fbd72c45a9ccb8
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-20 12:40:17 +0530

    form group

---
+++ internal/test/integration/molecules/form-group.fixture.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FormGroup, Form } from '@auth0/cosmos'
+
+class Fixture extends React.Component {
+  render() {
+    return (
+      <FormGroup id="custom-id">
+        <Form>
+          <Form.FieldSet label="iOS Settings">
+            <Form.TextInput label="Field label" type="text" placeholder="Enter something" />
+            <Form.Actions primaryAction={{ label: 'Save Changes', handler: () => {} }} />
+          </Form.FieldSet>
+        </Form>
+
+        <Form>
+          <Form.FieldSet label="Android Settings">
+            <Form.TextInput label="Field label" type="text" placeholder="Enter something" />
+            <Form.Actions primaryAction={{ label: 'Save Changes', handler: () => {} }} />
+          </Form.FieldSet>
+        </Form>
+      </FormGroup>
+    )
+  }
+}
+
+export default Fixture
