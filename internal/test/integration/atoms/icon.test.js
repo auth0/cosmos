f5976523e4c1420988a6983aa39bacbce2ca861d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-12-20 12:15:28 +0530

    folder structure

--- internal/test/integration/atoms/icon.test.js
+++ internal/test/integration/atoms/icon.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-testing-library'
 
 import Fixture from './icon.fixture'
-import customIdTest from './helpers/custom-id'
+import customIdTest from '../helpers/custom-id'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'icon')
