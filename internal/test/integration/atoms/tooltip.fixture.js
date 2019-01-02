d4b14a77543f6e1de4ea7c80861e53dada5551ae
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-02 12:56:00 +0530

    add tooltip tests

--- internal/test/integration/atoms/tooltip.fixture.js
+++ internal/test/integration/atoms/tooltip.fixture.js
@@ -4,7 +4,7 @@ import { Tooltip } from '@auth0/cosmos'
 class Fixture extends React.Component {
   render() {
     return (
-      <Tooltip id="custom-id" content="Here is some text" position="top">
+      <Tooltip id="custom-id" content="This is the tooltip text" position="top">
         Hover me
       </Tooltip>
     )
--- internal/test/integration/atoms/tooltip.test.js
+++ internal/test/integration/atoms/tooltip.test.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 
 import Fixture from './tooltip.fixture'
-import customIdTest from '../helpers/custom-id'
 
-test('Accepts custom id prop', () => {
-  customIdTest(Fixture, 'tooltip')
+test('Tooltip is hidden by default', () => {
+  const body = render(<Fixture />)
+
+  const tooltip = body.queryByTestId('tooltip')
+
+  expect(tooltip).toBeFalsy()
+})
+
+test('Tooltip should show up on hover', () => {
+  const body = render(<Fixture />)
+
+  const trigger = body.getByText('Hover me')
+
+  fireEvent.mouseEnter(trigger)
+  const tooltip = body.queryByTestId('tooltip')
+
+  expect(tooltip).toBeTruthy()
+  /* should have custom id as well */
+  expect(tooltip).toHaveAttribute('id', 'custom-id')
 })
