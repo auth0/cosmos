725b6d515c1fc15db85fac6d727b186b71e8e47b
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-07 16:46:04 +0530

    checkbox

--- internal/test/integration/atoms/checkbox.fixture.js
+++ internal/test/integration/atoms/checkbox.fixture.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Checkbox } from '@auth0/cosmos'
+import { mockFn } from '../helpers/event-handler'
 
 class Fixture extends React.Component {
   render() {
     return (
-      <Checkbox name="example" value="one" id="custom-id">
+      <Checkbox name="example" value="one" id="custom-id" onClick={mockFn}>
         You can check me
       </Checkbox>
     )
