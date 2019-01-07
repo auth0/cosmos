a7df8506afc0cdfeabbfbeca52f41944dff9537d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-07 16:51:29 +0530

    more atoms

--- internal/test/integration/atoms/icon.test.js
+++ internal/test/integration/atoms/icon.test.js
@@ -3,7 +3,12 @@ import { render } from 'react-testing-library'
 
 import Fixture from './icon.fixture'
 import customIdTest from '../helpers/custom-id'
+import eventHandlerTest from '../helpers/event-handler'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'icon')
+})
+
+test('Calls event handler', () => {
+  eventHandlerTest(Fixture, 'icon')
 })
--- internal/test/integration/atoms/label.test.js
+++ internal/test/integration/atoms/label.test.js
@@ -3,7 +3,12 @@ import { render } from 'react-testing-library'
 
 import Fixture from './label.fixture'
 import customIdTest from '../helpers/custom-id'
+import eventHandlerTest from '../helpers/event-handler'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'label')
+})
+
+test('Calls event handler', () => {
+  eventHandlerTest(Fixture, 'label')
 })
