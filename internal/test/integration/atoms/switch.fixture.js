641f8044e62a526c8aee56d5fa6213ad33ad05a9
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-07 17:25:34 +0530

    commit bug

--- internal/test/integration/atoms/switch.fixture.js
+++ internal/test/integration/atoms/switch.fixture.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Switch } from '@auth0/cosmos'
+import { mockFn } from '../helpers/event-handler'
+
+const onToggle = jest.fn()
 
 class Fixture extends React.Component {
   render() {
-    return <Switch id="custom-id" onToggle={value => console.log(value)} />
+    return <Switch id="custom-id" onToggle={onToggle} />
   }
 }
+
+Fixture.onToggle = onToggle
 
 export default Fixture
--- internal/test/integration/atoms/switch.test.js
+++ internal/test/integration/atoms/switch.test.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 
 import Fixture from './switch.fixture'
 import customIdTest from '../helpers/custom-id'
+import eventHandlerTest from '../helpers/event-handler'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'switch')
+})
+
+/*
+There's a bug here. onClick overwrites the onClick on Switch.Element
+*/
+test('Calls custom event handler', () => {
+  eventHandlerTest(Fixture, 'switch')
+})
+
+test('Calls onToggle', () => {
+  const body = render(<Fixture />)
+
+  const element = body.getByTestId('switch')
+
+  fireEvent.click(element)
+
+  expect(Fixture.customToggle).toHaveBeenCalledTimes(1)
 })
