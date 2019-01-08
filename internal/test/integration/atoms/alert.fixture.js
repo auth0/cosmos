88ae9b3ebfc03dae8c75803149768229bbe1062e
Franco Correa <franco@francocorrea.com>
2019-01-08 14:28:22 -0300

    Fix alert test

--- internal/test/integration/atoms/alert.fixture.js
+++ internal/test/integration/atoms/alert.fixture.js
@@ -5,11 +5,13 @@ import { mockFn } from '../helpers/event-handler'
 class Fixture extends React.Component {
   render() {
     return (
-      <Alert id="custom-id" icon="notes" type="default" title="FYI!" onDismiss={mockFn}>
+      <Alert id="custom-id" icon="notes" type="default" title="FYI!" onDismiss={Fixture.onDismiss}>
         Just a regular message
       </Alert>
     )
   }
 }
+
+Fixture.onDismiss = mockFn
 
 export default Fixture
