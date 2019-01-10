55a3417e38ea6dcaa782af4c112b176ca6cabf51
andresgalante <agalante@gmail.com>
2019-01-10 09:24:57 -0300

    update stories

--- core/components/molecules/form/field/stories/radio-field.story.js
+++ core/components/molecules/form/field/stories/radio-field.story.js
@@ -7,7 +7,7 @@ import { Form } from '@auth0/cosmos'
 storiesOf('Form', module).add('radio field', () => (
   <Example title="radio field">
     <Form>
-      <Form.Radio name="example1" selected="one" label="Form Label">
+      <Form.Radio name="example1" selected="one" label="Form label">
         <Form.Radio.Option value="one">One</Form.Radio.Option>
         <Form.Radio.Option value="two">Two</Form.Radio.Option>
         <Form.Radio.Option value="three">Three</Form.Radio.Option>
@@ -19,7 +19,7 @@ storiesOf('Form', module).add('radio field', () => (
 storiesOf('Form', module).add('radio field horizontal', () => (
   <Example title="radio field horizontal">
     <Form>
-      <Form.Radio name="example1" selected="one" align="horizontal" label="Form Label">
+      <Form.Radio name="example1" selected="one" align="horizontal" label="Form label">
         <Form.Radio.Option value="one">One</Form.Radio.Option>
         <Form.Radio.Option value="two">Two</Form.Radio.Option>
         <Form.Radio.Option value="three">Three</Form.Radio.Option>
@@ -35,7 +35,7 @@ storiesOf('Form', module).add('radio field + helper text', () => (
         name="example1"
         selected="one"
         helpText="Additional text to guide the user."
-        label="Form Label"
+        label="Form label"
       >
         <Form.Radio.Option value="one">One</Form.Radio.Option>
         <Form.Radio.Option value="two">Two</Form.Radio.Option>
@@ -48,7 +48,7 @@ storiesOf('Form', module).add('radio field + helper text', () => (
 storiesOf('Form', module).add('radio field + error', () => (
   <Example title="radio field + error">
     <Form>
-      <Form.Radio name="example1" selected="" error="Everything is broken" label="Form Label">
+      <Form.Radio name="example1" selected="" error="Everything is broken" label="Form label">
         <Form.Radio.Option value="one">One</Form.Radio.Option>
         <Form.Radio.Option value="two">Two</Form.Radio.Option>
         <Form.Radio.Option value="three">Three</Form.Radio.Option>
@@ -61,7 +61,7 @@ storiesOf('Form', module).add('radio field + error + helper text', () => (
   <Example title="radio field + error">
     <Form>
       <Form.Radio
-        label="Form Label"
+        label="Form label"
         name="example1"
         selected=""
         error="Everything is broken"
