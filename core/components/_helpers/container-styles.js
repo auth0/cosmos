d196afee4b7cba9804236126921f4ffdf2fac85d
Franco Correa <franco.correa@auth0.com>
2019-01-28 16:27:15 -0300

    Fix bullets in tabs (#1463)
    * Fix bullets in tabs
    
    * Update snapshots

--- core/components/_helpers/container-styles.js
+++ core/components/_helpers/container-styles.js
@@ -3,6 +3,16 @@ import { misc } from '@auth0/cosmos-tokens'
 
 const containerStyles = css`
   line-height: ${misc.lineHeight};
+
+  /**
+   * FIXME: Apps using Styleguide + Cosmos 
+   * don't pick up this from the globals.
+   * See: https://github.com/auth0/cosmos/issues/1460 
+   */
+  ul {
+    list-style: none;
+    padding: 0;
+  }
 `
 
 export default containerStyles
