62c8f4e5e8cadbe9069640aa0b64f752d81d8d00
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-28 17:14:53 +0530

    Base for Tokens v2 (#1431)
    * setup tokens v2
    
    * absorb v2 into same package
    
    * remove usage example

---
+++ core/tokens/v2/spacing.js
@@ -0,0 +1,13 @@
+const unit = 4
+
+const spacing = {
+  xxsmall: unit + 'px',
+  xsmall: unit * 2 + 'px',
+  small: unit * 4 + 'px',
+  medium: unit * 6 + 'px',
+  large: unit * 8 + 'px',
+  xlarge: unit * 10 + 'px',
+  xxlarge: unit * 14 + 'px'
+}
+
+export default spacing
