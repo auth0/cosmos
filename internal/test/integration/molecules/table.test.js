363f56bd733a7a7a891d82dc82cb55f3e8b23294
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-28 16:52:24 +0530

    table event handlers

--- internal/test/integration/molecules/table.test.js
+++ internal/test/integration/molecules/table.test.js
@@ -1,6 +1,26 @@
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
 import Fixture from './table.fixture'
 import customIdTest from '../helpers/custom-id'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'table')
+})
+
+test('Calls on sort event handler', () => {
+  const body = render(<Fixture />)
+
+  const nextButton = body.getByText('Goals')
+  fireEvent.click(nextButton)
+
+  expect(Fixture.onSort).toHaveBeenCalled()
+})
+
+test('Calls on row click event handler', () => {
+  const body = render(<Fixture />)
+
+  const row = body.getByTestId('table.row')
+  fireEvent.click(row)
+
+  expect(Fixture.onRowClick).toHaveBeenCalled()
 })
