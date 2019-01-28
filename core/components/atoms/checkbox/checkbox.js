a1193a6a1418a911cd8ddcb5b15ee8479e91d50d
Francisco Fernandes <1180964@isep.ipp.pt>
2019-01-28 18:00:39 +0000

    Changed const Label from p to div

--- core/components/atoms/checkbox/checkbox.js
+++ core/components/atoms/checkbox/checkbox.js
@@ -6,7 +6,7 @@ import Automation from '../../_helpers/automation-attribute'
 import containerStyles from '../../_helpers/container-styles'
 
 const CheckMark = styled.span``
-const Label = styled.p``
+const Label = styled.div``
 
 const justifyContent = {
   horizontal: `margin-right: ${spacing.medium}`,
@@ -64,7 +64,7 @@ Checkbox.Option = styled.label`
     height: 16px;
     width: 16px;
     background-color: ${props =>
-      props.readOnly ? colors.radio.backgroundDisabled : colors.radio.background};
+    props.readOnly ? colors.radio.backgroundDisabled : colors.radio.background};
     border: 1px solid
       ${props => (props.readOnly ? colors.radio.borderDisabled : colors.radio.border)};
     box-shadow: inset 0 1px 2px 0
