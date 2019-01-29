94222c03e70eb225f70ae769261c1029ac9a0db0
Francisco Fernandes <1180964@isep.ipp.pt>
2019-01-29 18:32:53 +0000

    Add another test to checkbox

--- core/components/atoms/checkbox/checkbox.story.js
+++ core/components/atoms/checkbox/checkbox.story.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Example, Stack } from '@auth0/cosmos/_helpers/story-helpers'
 
-import { Checkbox } from '@auth0/cosmos'
+import { Checkbox, Paragraph } from '@auth0/cosmos'
 
 const CheckBoxExample = () => (
   <Checkbox.Group name="example1" selected={['one', 'two']}>
@@ -43,5 +43,15 @@ storiesOf('Checkbox', module).add('long label', () => (
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur vestibulum sapien, lacinia vulputate purus porttitor sit amet. Etiam tincidunt eget diam at semper. Nulla pharetra odio vitae pharetra aliquet. Cras enim quam, maximus et molestie ut, rutrum vitae lacus. Pellentesque pretium mi et sapien varius elementum. Phasellus egestas condimentum tortor vel eleifend. Fusce lobortis varius mattis. Nam varius velit at quam rhoncus, a efficitur elit ultrices. Donec sollicitudin auctor tincidunt. In congue molestie nisi id egestas. Phasellus ac sodales leo. Vestibulum nec faucibus nibh, in efficitur urna. Fusce fermentum convallis condimentum. (long label)
       </Checkbox>
     </Checkbox.Group>
+  </Example>
+))
+
+storiesOf('Checkbox', module).add('single checkbox w/ long label', () => (
+  <Example>
+    <Paragraph>This is one paragraph!</Paragraph>
+    <Paragraph>This is another paragraph!</Paragraph>
+    <Checkbox name="one" value="one">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur vestibulum sapien, lacinia vulputate purus porttitor sit amet. Etiam tincidunt eget diam at semper. Nulla pharetra odio vitae pharetra aliquet. Cras enim quam, maximus et molestie ut, rutrum vitae lacus. Pellentesque pretium mi et sapien varius elementum. Phasellus egestas condimentum tortor vel eleifend. Fusce lobortis varius mattis. Nam varius velit at quam rhoncus, a efficitur elit ultrices. Donec sollicitudin auctor tincidunt. In congue molestie nisi id egestas. Phasellus ac sodales leo. Vestibulum nec faucibus nibh, in efficitur urna. Fusce fermentum convallis condimentum. (long label)
+    </Checkbox>
   </Example>
 ))
