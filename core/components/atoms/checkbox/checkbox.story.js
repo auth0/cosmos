84d8525ee186beebcb29987c66192ec7cb2bda34
Francisco Fernandes <1180964@isep.ipp.pt>
2019-01-29 13:44:02 +0000

    Added storybooks for checkbox and radio

--- core/components/atoms/checkbox/checkbox.story.js
+++ core/components/atoms/checkbox/checkbox.story.js
@@ -32,3 +32,16 @@ storiesOf('Checkbox', module).add('dark', () => (
     <CheckBoxExample />
   </Example>
 ))
+
+storiesOf('Checkbox', module).add('long label', () => (
+  <Example>
+    <Checkbox.Group name="example1" selected={['one']}>
+      <Checkbox name="one" value="one">
+        Option 1 (short label)
+      </Checkbox>
+      <Checkbox name="two" value="two">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur vestibulum sapien, lacinia vulputate purus porttitor sit amet. Etiam tincidunt eget diam at semper. Nulla pharetra odio vitae pharetra aliquet. Cras enim quam, maximus et molestie ut, rutrum vitae lacus. Pellentesque pretium mi et sapien varius elementum. Phasellus egestas condimentum tortor vel eleifend. Fusce lobortis varius mattis. Nam varius velit at quam rhoncus, a efficitur elit ultrices. Donec sollicitudin auctor tincidunt. In congue molestie nisi id egestas. Phasellus ac sodales leo. Vestibulum nec faucibus nibh, in efficitur urna. Fusce fermentum convallis condimentum. (long label)
+      </Checkbox>
+    </Checkbox.Group>
+  </Example>
+))
