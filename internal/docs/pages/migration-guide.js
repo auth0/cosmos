005fead96f2b1d15e6dc0673ab819be887ece93e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-02-06 16:23:54 +0530

    better looking guide

--- internal/docs/pages/migration-guide.js
+++ internal/docs/pages/migration-guide.js
@@ -2,49 +2,127 @@ import React from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 
-import {
-  Heading1,
-  Heading2,
-  Heading3,
-  Subheader,
-  Text,
-  List,
-  ListItem
-} from '../docs-components/typography'
+import { Heading1, Heading2, Heading3, Subheader, Text, Link } from '../docs-components/typography'
 
 import Tag from '../docs-components/tag'
-import { Code } from '@auth0/cosmos'
+import CodeBlock from '../docs-components/code-block'
 
 class MigrationGuide extends React.Component {
   render() {
     return (
       <div>
         <Helmet title="Migration guide &ndash; Cosmos" />
-
         <Heading1>
           Migrating to 1.0.0 <Tag text="Work in progress" />
         </Heading1>
         <Subheader>This is a work in progress to help migrate to 1.0.0</Subheader>
-
         <Heading2>API Changes</Heading2>
 
-        <Heading3>Alert</Heading3>
-        <List>
-          <ListItem>
-            <Text>
-              Replace <Code>text:string</Code> with <Code>children:string</Code>
-            </Text>
-          </ListItem>
-        </List>
+        <div>
+          <Heading3>Alert</Heading3>
 
-        <Heading3>TextInput</Heading3>
-        <List>
-          <ListItem>
-            <Text>
-              Replace <Code>error:string</Code> with <Code>hasError:boolean</Code>
-            </Text>
-          </ListItem>
-        </List>
+          <Link href="#/component/alert">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace text:string
+<Alert text="This is important" />
+
+// with children:string
+<Alert>This is important</Alert>
+        `}
+          </CodeBlock>
+        </div>
+
+        <div>
+          <Heading3>TextInput</Heading3>
+
+          <Link href="#/component/text-input">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace error:string
+<TextInput error="Please enter name" />
+
+// with hasError:boolean
+<TextInput hasError={true} />
+        `}
+          </CodeBlock>
+        </div>
+
+        <div>
+          <Heading3>TextArea</Heading3>
+
+          <Link href="#/component/text-area">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace error:string
+<TextInput error="Please enter name" />
+
+// with hasError:boolean
+<TextInput hasError={true} />
+        `}
+          </CodeBlock>
+        </div>
+
+        <div>
+          <Heading3>AvatarBlock</Heading3>
+
+          <Link href="#/component/avatar-block">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace href:string
+<AvatarBlock href="https://auth0.com" title="Title with link"/>
+
+// with link:shape
+<AvatarBlock link={{ href: 'https://auth0.com', target:'_blank' }} title="Title with link"/>
+        `}
+          </CodeBlock>
+        </div>
+
+        <div>
+          <Heading3>EmptyState</Heading3>
+
+          <Link href="#/component/empty-state">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace text:string
+<EmptyState text="No items have been added to this section." />
+
+// with children:element
+<EmptyState>No items have been added to this section</EmptyState>
+        `}
+          </CodeBlock>
+        </div>
+
+        <div>
+          <Heading3>ResourceList</Heading3>
+
+          <Link href="#/component/resource-list">Open docs</Link>
+
+          <CodeBlock language="jsx">
+            {`
+// replace actions:[shape]
+<ResourceList
+  actions={[
+    { icon:"settings", label: "Settings", onClick={fn} },
+    { icon:"delete",   label: "Delete", onClick={fn} }
+  ]}
+/>
+
+// with actions:[elements]
+<ResourceList
+  actions={[
+    <Button icon="settings" onClick={fn}>Settings</Button>,
+    <Button icon="delete"   onClick={fn}>Delete</Button>
+  ]}
+/>
+        `}
+          </CodeBlock>
+        </div>
 
         <br />
         <br />
