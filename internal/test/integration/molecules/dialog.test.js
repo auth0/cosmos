746f05d8d7bba3efb3e4ca57ed8a71eda482d99b
Franco Correa <franco.correa@auth0.com>
2019-02-07 21:49:28 -0300

    Revert "[DXDP-88] Composable actions for Tabs in Dialog  (#1458)" (#1496)
    This reverts commit 4771134f9a48b43491f0aac099c3f0e54b242e89.

--- internal/test/integration/molecules/dialog.test.js
+++ internal/test/integration/molecules/dialog.test.js
@@ -2,15 +2,10 @@ import React from 'react'
 import { render } from 'react-testing-library'
 
 import Fixture from './dialog.fixture'
-import ComposedFixture from './dialog-composed.fixture'
 import customIdTest from '../helpers/custom-id'
 
 test('Accepts custom id prop', () => {
   customIdTest(Fixture, 'dialog')
-})
-
-test('Accepts custom id prop - composed', () => {
-  customIdTest(ComposedFixture, 'dialog')
 })
 
 test('Calls actions handler', () => {
@@ -22,15 +17,6 @@ test('Calls actions handler', () => {
   expect(Fixture.actionOnClick).toHaveBeenCalled()
 })
 
-test('Calls actions handler - composed', () => {
-  const dialog = render(<ComposedFixture />)
-
-  const dialogAction = dialog.getAllByTestId('dialog.action')[0]
-  dialogAction.click()
-
-  expect(ComposedFixture.actionOnClick).toHaveBeenCalled()
-})
-
 test('Calls onClose handler', () => {
   const dialog = render(<Fixture />)
 
@@ -38,13 +24,4 @@ test('Calls onClose handler', () => {
   closeAction.click()
 
   expect(Fixture.onClose).toHaveBeenCalled()
-})
-
-test('Calls onClose handler - composed', () => {
-  const dialog = render(<ComposedFixture />)
-
-  const closeAction = dialog.getByTestId('dialog.close')
-  closeAction.click()
-
-  expect(ComposedFixture.onClose).toHaveBeenCalled()
 })
