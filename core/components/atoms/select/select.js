6626b7e28793679533c3cd41e65905de11927fbf
Franco Correa <franco@francocorrea.com>
2019-02-12 17:37:05 -0300

    Basic implementation

--- core/components/atoms/select/select.js
+++ core/components/atoms/select/select.js
@@ -2,14 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Automation from '../../_helpers/automation-attribute'
 import Icon from '../icon'
+import Tag from '../tag'
+import Spinner from '../spinner'
 import styled from '@auth0/cosmos/styled'
+import ReactSelect, { defaultTheme } from 'react-select'
 
 import { misc, colors, spacing } from '@auth0/cosmos-tokens'
-import { StyledInput } from '../_styled-input'
 
 const selectOpacity = {
   default: 1,
   disabled: 0.5
+}
+
+const selectTheme = {
+  ...defaultTheme,
+  colors: {
+    ...defaultTheme.colors,
+    danger: colors.input.borderError,
+    primary: colors.input.borderFocus
+  },
+  borderRadius: misc.radius,
+  spacing: {
+    menuGutter: 4,
+    baseUnit: spacing.unit,
+    controlHeight: misc.input.default.height
+  }
+}
+
+const cosmosToReactSelect = {
+  options: cosmosOptions =>
+    cosmosOptions.map(({ disabled, label, text, ...otherProperties }) => ({
+      isDisabled: disabled,
+      label: label || text,
+      ...otherProperties
+    }))
 }
 
 const PLACEHOLDER_VALUE = '__select_placeholder'
@@ -39,75 +65,107 @@ const renderOption = (option, idx) => {
   )
 }
 
-const Select = ({ options, ...props }) => {
+const customOptionRenderer = providedRenderer => optionProps => {
+  const { innerProps, innerRef, data } = optionProps
+  const ifDisabled = (disabledValue, enabledValue) =>
+    data.isDisabled ? disabledValue : enabledValue
+
+  const state = { isHovered: optionProps.isFocused }
+  const opacity = selectOpacity[ifDisabled('disabled', 'default')]
+  const cursor = ifDisabled('no-drop', 'pointer')
+
+  const style = { width: '100%', opacity, cursor }
+
+  return (
+    <div ref={innerRef} {...innerProps} style={style}>
+      {providedRenderer(data, state)}
+    </div>
+  )
+}
+
+const cosmosMultiValueTagRenderer = optionProps => {
+  const { innerProps, removeProps, innerRef, data } = optionProps
+  console.log('tag', { optionProps })
+  return (
+    <Select.Tag {...innerProps} ref={innerRef} onRemove={removeProps.onClick}>
+      {data.label}
+    </Select.Tag>
+  )
+}
+
+const cosmosDownIndicator = ({ innerProps }) => (
+  <Select.ArrowIcon {...innerProps} name="dropdown-fill" size="14" color="default" />
+)
+
+const cosmosLoadingIndicator = () => <Select.Spinner />
+
+const Select = props => {
   /*
     select boxes do not support readonly like input boxes,
     but they do have disabled. we need the style of readOnly input
     and functionality of select disabled
   */
 
+  const componentOverrides = {
+    MultiValue: cosmosMultiValueTagRenderer,
+    DropdownIndicator: cosmosDownIndicator,
+    LoadingIndicator: cosmosLoadingIndicator,
+    IndicatorSeparator: () => null
+  }
+
+  let options = cosmosToReactSelect.options(props.options)
+
   const shouldUsePlaceholder = valueIsUndefined(props.value) && valueIsUndefined(props.defaultValue)
-  if (shouldUsePlaceholder) props.value = PLACEHOLDER_VALUE
+  const value = shouldUsePlaceholder ? PLACEHOLDER_VALUE : props.value
+
+  if (props.customOptionRenderer) {
+    componentOverrides.Option = customOptionRenderer(props.customOptionRenderer)
+  }
+
+  if (shouldUsePlaceholder) {
+    options = [{ label: props.placeholder, value: PLACEHOLDER_VALUE, isDisabled: true }, ...options]
+  }
 
   return (
-    <Select.Wrapper>
-      <Select.ArrowIcon name="dropdown-fill" size="14" color="default" />
-      <Select.Element {...props} {...Automation('select')}>
-        {/* First option will be selected if there is no value passed as a prop */}
-        <option disabled hidden value={PLACEHOLDER_VALUE} {...Automation('select.option')}>
-          {props.placeholder}
-        </option>
-
-        {options.map(renderOption)}
-      </Select.Element>
-    </Select.Wrapper>
+    <ReactSelect
+      onChange={option => props.onChange({ target: { value: option.value } })}
+      isDisabled={props.disabled}
+      isMulti={props.multiple}
+      isSearchable={props.searchable}
+      isLoading={props.loading}
+      menuIsOpen={props.defaultOpen}
+      options={options}
+      placeholder={props.placeholder}
+      components={componentOverrides}
+      theme={selectTheme}
+      {...Automation('select')}
+      // value={value}
+      defaultValue={props.defaultValue}
+    />
   )
 }
 
-Select.Element = styled(StyledInput.withComponent('select'))`
-  appearance: none;
-
-  padding-right: ${spacing.large};
-
-  height: ${misc.input.default.height};
-  opacity: ${props => (props.disabled ? selectOpacity.disabled : selectOpacity.default)};
-  background-color: ${props =>
-    props.disabled ? colors.input.backgroundReadOnly : colors.input.background};
-`
-
-Select.Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-`
-
 Select.ArrowIcon = styled(Icon)`
-  position: absolute;
-  right: 12px;
   pointer-events: none;
-
+  margin-right: ${spacing.unit * 3}px;
   svg {
     display: block;
   }
 `
 
-const selectOptionShape = PropTypes.shape({
-  text: PropTypes.string.isRequired,
-  value: PropTypes.any.isRequired,
-  disabled: PropTypes.bool
-})
+Select.Spinner = styled(Spinner)`
+  margin-right: ${spacing.xsmall};
+`
+
+Select.Tag = styled(Tag)`
+  margin-top: calc(${spacing.xxsmall} / 2);
+  margin-bottom: calc(${spacing.xxsmall} / 2);
+  margin-right: ${spacing.xxsmall};
+`
 
 Select.propTypes = {
   /** Options to render inside select */
-  options: PropTypes.arrayOf(
-    PropTypes.oneOfType([
-      selectOptionShape,
-      PropTypes.shape({
-        groupName: PropTypes.string.isRequired,
-        items: PropTypes.arrayOf(selectOptionShape)
-      })
-    ])
-  ),
+  options: PropTypes.arrayOf(PropTypes.object),
   /** Value selected by default */
   value: PropTypes.any,
   /** onChange transparently passed to select */
@@ -115,12 +173,21 @@ Select.propTypes = {
   /** String to show when the first empty choice is selected */
   placeholder: PropTypes.string,
   /** Determines if the select should be disabled */
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  /** Determines if the user can type to search for items */
+  searchable: PropTypes.bool,
+  /** Determines if the user can select more than one item */
+  multiple: PropTypes.bool,
+  /** Shows a spinner inside the select control */
+  loading: PropTypes.bool,
+  /** Shows a cross icon that clears the select */
+  clearable: PropTypes.bool
 }
 
 Select.defaultProps = {
   options: [],
-  placeholder: ''
+  placeholder: '',
+  searchable: false
 }
 
 export default Select
