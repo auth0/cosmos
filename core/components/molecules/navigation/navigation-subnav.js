9418364d8f0e19b02b9b37999e79345e3e8718df
Andres Galante <agalante@gmail.com>
2019-03-07 15:22:14 -0300

    [DXDP-141] Creates new navigation component (#1539)
    * adds navigation component
    
    * improves spaces on navigation
    
    * Token adjustments
    
    * removes focus from navigation links by adding a default href to the links
    
    * Add snapshot tests
    
    * Remove label as a required prop in Navigation.Item
    
    * Add required description to Story navigations

---
+++ core/components/molecules/navigation/navigation-subnav.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from '@auth0/cosmos/styled'
+import PropTypes from 'prop-types'
+import Icon, { __ICONNAMES__ } from '../../atoms/icon'
+import NavigationItem from './navigation-item'
+
+import { spacing, colors } from '@auth0/cosmos-tokens'
+
+import Automation from '../../_helpers/automation-attribute'
+
+const passSubMenuPropToChildren = children =>
+  React.Children.map(children, el => React.cloneElement(el, { inSubMenu: true }))
+
+const NavigationSubnav = props => {
+  const { children, open } = props
+
+  return (
+    <NavigationSubnav.Content {...Automation('navigation.link-group')} open={open} hidden={!open}>
+      {passSubMenuPropToChildren(children)}
+    </NavigationSubnav.Content>
+  )
+}
+
+NavigationSubnav.Content = styled.ul`
+  flex: 1 0 100%;
+  margin-left: calc(18px + ${spacing.xsmall});
+  overflow: hidden;
+  max-height: ${props => (props.open ? props.children.length * 50 + 'px' : '0')};
+  visibility: ${props => (props.open ? 'visible' : 'hidden')};
+  transition: all 0.3s ease-in-out;
+
+  ${NavigationItem} {
+    a {
+      padding-top: ${spacing.xxsmall};
+      padding-bottom: ${spacing.xxsmall};
+    }
+  }
+  ${Icon.Element} svg {
+    width: 16px;
+    height: 16px;
+  }
+`
+
+NavigationSubnav.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+NavigationSubnav.defaultProps = {}
+
+export default NavigationSubnav
