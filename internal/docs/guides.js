525a7f83315f92b4b8d128f69673f31e7483a5a8
Franco Correa <franco@francocorrea.com>
2019-03-08 13:44:56 -0300

    Remove automation glossary

--- internal/docs/guides.js
+++ internal/docs/guides.js
@@ -3,7 +3,6 @@ import Usage from './guides/usage'
 import ContributionGuide from './guides/contribution-guide'
 import FAQS from './guides/faqs'
 import Changes from './guides/changes'
-import AutomationGlossary from './guides/automation-glossary'
 import MigrationGuide from './guides/migration-guide'
 import GettingStarted from './guides/getting-started'
 
@@ -13,7 +12,6 @@ const guides = [
   { path: '/guiding-principles', title: 'Guiding Principles', component: GuidingPrinciples },
   { path: '/faqs', title: 'FAQs', component: FAQS },
   { path: '/changes', title: 'Changelog', component: Changes },
-  { path: '/automation', title: 'Automation Glossary', component: AutomationGlossary },
   { path: '/contribution-guide', title: 'Contributing to Cosmos', component: ContributionGuide },
   { path: '/migration-guide', title: 'Migration Guide', component: MigrationGuide }
 ]
