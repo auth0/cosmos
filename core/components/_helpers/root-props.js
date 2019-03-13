1dd19cd6329ce2226190fb56e2b92668dff70788
Franco Correa <franco@francocorrea.com>
2019-03-13 15:54:20 -0300

    Add rootProps helper

---
+++ core/components/_helpers/root-props.js
@@ -0,0 +1,17 @@
+/**
+ * Avoid passing down native HTML props to the root element of a component.
+ *
+ * For example, when you use Dialog you type something like this:
+ *      <Dialog title="This is a title"> ... </Dialog>
+ *
+ * If we just do {...props} in the root element of the component, you
+ * get something the `title` prop passed to the native HTML element,
+ * and this causes a tooltip to appear in the element when you rest
+ * the cursor on it.
+ *
+ * @param {object} props
+ */
+export const rootProps = props => {
+  const { title, ...rest } = props
+  return rest
+}
