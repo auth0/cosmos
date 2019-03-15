1b9f31d6e6480d0a773a44c61ffd33c415ff0dda
Franco Correa <franco@francocorrea.com>
2019-03-15 12:01:00 -0300

    Move placeholders to avatar url

--- core/components/_helpers/avatar-url.js
+++ core/components/_helpers/avatar-url.js
@@ -1,5 +1,10 @@
 const md5 = require('md5')
 
+const PLACEHOLDERS = {
+  user: 'https://cdn.auth0.com/website/cosmos/avatar-user-default.svg',
+  resource: 'https://cdn.auth0.com/website/cosmos/avatar-resource-default.svg'
+}
+
 const initialsAvatar = initials =>
   initials
     ? encodeURIComponent(`https://cdn.auth0.com/avatars/${initials.toLowerCase()}.png`)
@@ -8,9 +13,8 @@ const initialsAvatar = initials =>
 const urlTemplate = (hash, initials, size) =>
   `https://s.gravatar.com/avatar/${hash}?s=${size}&r=pg&d=${initialsAvatar(initials)}`
 
-export default function avatarUrl(email, initials, size = 480) {
-  if (!email && !initials)
-    throw new Error('You need to provide either `email`, `initials` or both to the Avatar.')
+export default function avatarUrl(email, initials, type = 'user', size = 480) {
+  if (typeof email !== 'string' && typeof initials !== 'string') return PLACEHOLDERS[type]
 
   const hash = email ? md5(email.toLowerCase()) : ''
   return urlTemplate(hash, initials, size)
