3f4b902f779ff3314862afa74f6a5d3973153613
Franco Correa <franco@francocorrea.com>
2019-03-15 12:01:06 -0300

    Add tests

--- internal/test/unit/helpers/avatar-url.test.js
+++ internal/test/unit/helpers/avatar-url.test.js
@@ -1,13 +1,6 @@
 import avatarUrl from '@auth0/cosmos/_helpers/avatar-url'
 
 describe('Avatar URL', () => {
-  it('to throw if no arguments are passed', () => {
-    const url = () => avatarUrl()
-    expect(url).toThrowError(
-      'You need to provide either `email`, `initials` or both to the Avatar.'
-    )
-  })
-
   it('to return an initials URL if only initials is provided', () => {
     const url = avatarUrl(null, 'AB')
     expect(url).toEqual(
@@ -30,9 +23,34 @@ describe('Avatar URL', () => {
   })
 
   it('to return a custom size URL if size is provided', () => {
-    const url = avatarUrl('example@example.com', 'FC', 368)
+    const url = avatarUrl('example@example.com', 'FC', null, 368)
     expect(url).toEqual(
       'https://s.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8?s=368&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Ffc.png'
     )
+  })
+
+  it('to return a placeholder URL if no data is provided', () => {
+    const url = avatarUrl()
+    expect(url).toEqual('https://cdn.auth0.com/website/cosmos/avatar-user-default.svg')
+  })
+
+  it('to return a placeholder URL if no data but type (user) is provided', () => {
+    const url = avatarUrl(null, null, 'user')
+    expect(url).toEqual('https://cdn.auth0.com/website/cosmos/avatar-user-default.svg')
+  })
+
+  it('to return a placeholder URL if no data but type (resource) is provided', () => {
+    const url = avatarUrl(null, null, 'resource')
+    expect(url).toEqual('https://cdn.auth0.com/website/cosmos/avatar-resource-default.svg')
+  })
+
+  it('to return a placeholder URL if email is not a string', () => {
+    const url = avatarUrl(true)
+    expect(url).toEqual('https://cdn.auth0.com/website/cosmos/avatar-user-default.svg')
+  })
+
+  it('to return a placeholder URL if initials is not a string', () => {
+    const url = avatarUrl(null, true)
+    expect(url).toEqual('https://cdn.auth0.com/website/cosmos/avatar-user-default.svg')
   })
 })
