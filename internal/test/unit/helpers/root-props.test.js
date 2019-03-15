76d6302eb3d3a65a5a998b0e5b379e892467f153
Franco Correa <franco@francocorrea.com>
2019-03-15 14:57:03 -0300

    Add more test cases to root props helper

--- internal/test/unit/helpers/root-props.test.js
+++ internal/test/unit/helpers/root-props.test.js
@@ -6,4 +6,22 @@ describe('rootProps', () => {
     const validProps = rootProps(props)
     expect(validProps).toEqual({ otherProp: 1, anotherProp: 'abc' })
   })
+
+  it('returns an empty object if an empty object is provided', () => {
+    const props = {}
+    const validProps = rootProps(props)
+    expect(validProps).toEqual({})
+  })
+
+  it('returns an empty object if an only title is provided', () => {
+    const props = { title: 'Dialog' }
+    const validProps = rootProps(props)
+    expect(validProps).toEqual({})
+  })
+
+  it('returns the same object if an title is not present', () => {
+    const props = { otherProp: 1, anotherProp: 'abc' }
+    const validProps = rootProps(props)
+    expect(validProps).toEqual({ otherProp: 1, anotherProp: 'abc' })
+  })
 })
