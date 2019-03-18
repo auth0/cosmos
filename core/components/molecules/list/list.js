4aa8a34c70bf09f2efb883c7527d11f2f760f91a
Franco Correa <franco@francocorrea.com>
2019-03-18 11:28:44 -0300

    Retrigger CI

--- core/components/molecules/list/list.js
+++ core/components/molecules/list/list.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import { SortableContainer, SortableElement, SortableHandle, arrayMove } from 'react-sortable-hoc'
 import Automation from '../../_helpers/automation-attribute'
 import containerStyles from '../../_helpers/container-styles'
-
 import { colors, spacing } from '@auth0/cosmos-tokens'
 import Heading, { StyledHeading } from '../../atoms/heading'
 import Icon from '../../atoms/icon'
