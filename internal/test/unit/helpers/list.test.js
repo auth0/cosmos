f1f13907ccade2a7b5d6b4b959e6e4bad04231e9
Franco Correa <franco@francocorrea.com>
2019-03-20 15:30:26 -0300

    Add tests :white_check_mark:

--- internal/test/unit/helpers/list.test.js
+++ internal/test/unit/helpers/list.test.js
@@ -1,53 +1,87 @@
 import React from 'react'
-import { getDrawer } from '@auth0/cosmos/_helpers/list'
+import { shallow } from 'enzyme'
+import { getDrawer, isListExpandable, excludeDrawer } from '@auth0/cosmos/_helpers/list'
 import { List } from '@auth0/cosmos'
 
-describe('List', () => {
-  it('List can handle List.Drawer', () => {
-    const providedDrawer = (
+const testCases = {
+  includingListDrawer: () => (
+    <List.Item>
       <List.Drawer description="Something">
         <p>This is a test</p>
       </List.Drawer>
-    )
-
-    const children = <List.Item>{providedDrawer}</List.Item>
-
-    const drawer = getDrawer(children, false, List.Drawer)
-    expect(drawer[0].type).toEqual(List.Drawer)
-  })
-
-  it('List can handle', () => {
-    const providedChildren = (
+    </List.Item>
+  ),
+  excludingListDrawer: () => (
+    <List.Item>
       <List.Body>
         <span>ID:</span>
       </List.Body>
-    )
+    </List.Item>
+  ),
+  nullChildren: () => <List.Item>{null}</List.Item>
+}
 
-    const children = <List.Item>{providedChildren}</List.Item>
+describe('getDrawer', () => {
+  it('should handle a List.Drawer', () => {
+    const drawer = getDrawer(testCases.includingListDrawer(), false, List.Drawer)
+    expect(drawer[0].type).toEqual(List.Drawer)
+  })
 
-    const drawer = getDrawer(children, false, List.Drawer)
+  it('should handle children without List.Drawer', () => {
+    const drawer = getDrawer(testCases.excludingListDrawer(), false, List.Drawer)
     expect(drawer).toHaveLength(0)
   })
 
-  it('List can handle', () => {
-    const providedChildren = null
-
-    const children = <List.Item>{providedChildren}</List.Item>
-
-    const drawer = getDrawer(children, false, List.Drawer)
+  it('should handle null children', () => {
+    const drawer = getDrawer(testCases.nullChildren(), false, List.Drawer)
     expect(drawer).toBeNull()
   })
 
-  it('List can handle List.Drawer', () => {
-    const providedDrawer = (
-      <List.Drawer description="Something">
-        <p>This is a test</p>
-      </List.Drawer>
-    )
-
-    const children = <List.Item>{providedDrawer}</List.Item>
-
-    const drawer = getDrawer(children, false, List.Drawer)
+  it('should pass a hidden prop to List.Drawer', () => {
+    const drawer = getDrawer(testCases.includingListDrawer(), false, List.Drawer)
     expect(drawer[0].props.hidden).toEqual(true)
+  })
+})
+
+describe('isListExpandable', () => {
+  it('should handle a List.Drawer', () => {
+    const [isExpandable, _] = isListExpandable(testCases.includingListDrawer(), List.Drawer)
+
+    expect(isExpandable).toBe(true)
+  })
+
+  it('should handle children without List.Drawer', () => {
+    const [isExpandable, _] = isListExpandable(testCases.excludingListDrawer(), List.Drawer)
+
+    expect(isExpandable).toBe(false)
+  })
+
+  it('should handle null children', () => {
+    const [isExpandable, _] = isListExpandable(testCases.nullChildren(), List.Drawer)
+
+    expect(isExpandable).toBe(false)
+  })
+})
+
+describe('excludeDrawer', () => {
+  it('should handle a List.Drawer', () => {
+    const result = excludeDrawer(testCases.includingListDrawer(), List.Drawer)
+    const children = shallow(result).children()
+
+    expect(children.exists()).toBe(false)
+  })
+
+  it('should handle children without List.Drawer', () => {
+    const result = excludeDrawer(testCases.excludingListDrawer(), List.Drawer)
+    const children = shallow(result).children()
+
+    expect(children.exists()).toBe(true)
+  })
+
+  it('should handle null children', () => {
+    const result = excludeDrawer(testCases.nullChildren(), List.Drawer)
+    const children = shallow(result).children()
+
+    expect(children.exists()).toBe(false)
   })
 })
