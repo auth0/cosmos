956501821f2da5647f080ad8cf8fa899f6a6ec67
Franco Correa <franco.correa@auth0.com>
2019-03-21 15:30:55 -0300

    Do not pass react-select props to select's loading indicator (#1574)

--- core/components/atoms/select/components/loading-indicator.js
+++ core/components/atoms/select/components/loading-indicator.js
@@ -1,7 +1,10 @@
+import React from 'react'
 import styled from '@auth0/cosmos/styled'
 import { spacing } from '@auth0/cosmos-tokens'
 import Spinner from '../../spinner'
 
-export const LoadingIndicator = styled(Spinner)`
+export const LoadingIndicator = () => <LoadingIndicator.Element />
+
+LoadingIndicator.Element = styled(Spinner)`
   margin-right: ${spacing.xsmall};
 `
