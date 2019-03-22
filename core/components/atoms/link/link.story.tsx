a655caa4aeb9bcf1770a1a85295b6f3a61b48b5a
Franco Correa <franco@francocorrea.com>
2019-03-22 17:45:43 -0300

    Link migrated to TS

--- core/components/atoms/link/link.story.tsx
+++ core/components/atoms/link/link.story.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import * as React from 'react'
 import { storiesOf } from '@storybook/react'
-import { Example, Stack } from '@auth0/cosmos/_helpers/story-helpers'
+import { Example } from '@auth0/cosmos/_helpers/story-helpers'
 
 import { Link } from '@auth0/cosmos'
 
