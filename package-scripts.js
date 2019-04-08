1b5db71707645d41ac77ea31d39a786d7b22d83a
Franco Correa <franco@francocorrea.com>
2019-04-08 18:11:36 -0300

    Test

--- package-scripts.js
+++ package-scripts.js
@@ -41,9 +41,17 @@ module.exports = {
       }
     },
     test: {
+      ciunit: {
+        script: series('test.unit'),
+      },
+      ciintegration: {
+        script: series('test.integration'),
+      },
+      civisual: {
+        script: series('test.visual'),
+      },
       default: {
-        script: series('production.build', 'test.unit', 'test.integration'),
-        // script: series('production.build', 'test.visual', 'test.unit', 'test.integration'),
+        script: series('production.build'),
         description:
           'Check if applications build + Run visual tests + Run unit and integration tests'
       },
