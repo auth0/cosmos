e50d5ce894dc21df5b019c7c5fa542efb92d16e5
dasha <dasha.nikitina@auth0.com>
2019-07-11 16:03:06 -0600

    update onchange and add loading state

--- core/components/atoms/file-input/file-input.story.tsx
+++ core/components/atoms/file-input/file-input.story.tsx
@@ -1,23 +1,49 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Example } from '../../_helpers/story-helpers'
-import FileInput from '.';
-
+import FileInput from '.'
 
 storiesOf('FileInput', module).add('simple', () => (
   <Example title="simple">
-    <FileInput files={[{ name: 'file1.txt', size: 3579 }]} />
+    <FileInput items={[{ file: { name: 'file1.txt', size: 3579 } }]} />
   </Example>
 ))
 
 storiesOf('FileInput', module).add('multiple files', () => (
   <Example title="simple">
-    <FileInput files={[{ name: 'file1.txt', size: 3579 }, { name: 'file2.txt', size: 12356 }]} multiple />
+    <FileInput
+      items={[
+        { file: { name: 'file1.txt', size: 3579 } },
+        { file: { name: 'file2.txt', size: 12356 } }
+      ]}
+      multiple
+    />
+  </Example>
+))
+
+storiesOf('FileInput', module).add('loading state', () => (
+  <Example title="simple">
+    <FileInput
+      items={[
+        { file: { name: 'file1.txt', size: 3579 }, loading: true },
+        { file: { name: 'file2.txt', size: 12356 } }
+      ]}
+      multiple
+    />
   </Example>
 ))
 
 storiesOf('FileInput', module).add('file truncation', () => (
   <Example title="simple">
-    <FileInput files={[{ name: 'this-file-is-more-than-50-characters-long-so-we-need-to-truncate.txt', size: 99887 }]} />
+    <FileInput
+      items={[
+        {
+          file: {
+            name: 'this-file-is-more-than-50-characters-long-so-we-need-to-truncate.txt',
+            size: 99887
+          }
+        }
+      ]}
+    />
   </Example>
 ))
