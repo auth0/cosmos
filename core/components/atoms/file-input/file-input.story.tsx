a8585fb6100399e9be85ccb02a8cd18a81d2c204
Dan <dan.arias@auth0.com>
2019-09-19 17:37:56 -0400

    Apple Prettier formatting

--- core/components/atoms/file-input/file-input.story.tsx
+++ core/components/atoms/file-input/file-input.story.tsx
@@ -1,51 +1,45 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { storiesOf } from '@storybook/react'
+import { storiesOf } from "@storybook/react";
 
-import { Example } from '../../_helpers/story-helpers'
-import FileInput from './'
+import { Example } from "../../_helpers/story-helpers";
+import FileInput from "./";
 
-storiesOf('FileInput', module).add('simple', () => (
+storiesOf("FileInput", module).add("simple", () => (
   <Example title="simple">
-    <FileInput items={[{ file: { name: 'file1.txt', size: 3579 } }]} />
+    <FileInput items={[{ file: { name: "file1.txt", size: 3579 } }]} />
   </Example>
-))
+));
 
-storiesOf('FileInput', module).add('multiple files', () => (
+storiesOf("FileInput", module).add("multiple files", () => (
   <Example title="multiple files">
     <FileInput
-      items={[
-        { file: { name: 'file1.txt', size: 3579 } },
-        { file: { name: 'file2.txt', size: 12356 } }
-      ]}
+      items={[{ file: { name: "file1.txt", size: 3579 } }, { file: { name: "file2.txt", size: 12356 } }]}
       multiple
     />
   </Example>
-))
+));
 
-storiesOf('FileInput', module).add('loading state', () => (
+storiesOf("FileInput", module).add("loading state", () => (
   <Example title="loading state">
     <FileInput
-      items={[
-        { file: { name: 'file1.txt', size: 3579 }, loading: true },
-        { file: { name: 'file2.txt', size: 12356 } }
-      ]}
+      items={[{ file: { name: "file1.txt", size: 3579 }, loading: true }, { file: { name: "file2.txt", size: 12356 } }]}
       multiple
     />
   </Example>
-))
+));
 
-storiesOf('FileInput', module).add('file name truncation', () => (
+storiesOf("FileInput", module).add("file name truncation", () => (
   <Example title="file name truncation">
     <FileInput
       items={[
         {
           file: {
-            name: 'this-file-is-more-than-50-characters-long-so-we-need-to-truncate.txt',
-            size: 99887
-          }
-        }
+            name: "this-file-is-more-than-50-characters-long-so-we-need-to-truncate.txt",
+            size: 99887,
+          },
+        },
       ]}
     />
   </Example>
-))
+));
