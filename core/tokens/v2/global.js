d5e81ed711b7292f16e185234548be464cfafde1
Fernando Carrettoni <fcarrettoni@gmail.com>
2020-01-14 11:58:14 -0300

    Improved variables names

--- core/tokens/v2/global.js
+++ core/tokens/v2/global.js
@@ -1,12 +1,12 @@
-import { swatches as swatch } from './swatches'
+import { swatches as swatch } from "./swatches";
 
 /*
 // Tokens ==========================
 
 Color
 - Background
-- Border
 - Text
+- Link
 - Theme
 -- Brand
 -- Primary
@@ -15,11 +15,13 @@ Color
 -- Warning
 -- Danger
 
-Size
-
+Spacing
+Gutter
+Height
 
 Border
 - width
+- color
 - radius
 
 Font
@@ -40,129 +42,123 @@ const global = {
   // Colors ==========================
   color: {
     background: {
-      base: {
-        100: swatch.black - 100,
-        200: '',
-        300: ''
+      // Default calls light colors
+      default: {
+        100: "swatch.black.100",
+        200: "",
+        300: "",
       },
       light: {
-        100: '',
-        200: '',
-        300: ''
+        100: "", // Form bg
+        200: "",
+        300: "",
       },
       dark: {
-        100: '',
-        200: '',
-        300: ''
-      }
-    },
-    border: {
-      base: '#', // TODO: this should be just `global.border.color` widthout the word default
-      light: '#',
-      dark: '#'
+        100: "",
+        200: "",
+        300: "", // Tooltip bg
+      },
     },
     text: {
-      base: {
-        300: 'text.dark.300', // Primary
-        200: 'text.dark.200', // Secondary
-        100: 'text.dark.100' // Disabled
-        // Error
-        // Interactive/Link
-        // Interactive/Link Dark
-      },
-      light: {
-        300: '#',
-        200: '#',
-        100: '#'
+      // Default calls dark colors
+      default: {
+        100: "text.dark.100", // Disabled
+        200: "text.dark.200", // Secondary
+        300: "text.dark.300", // Primary
       },
       dark: {
-        300: '#333333',
-        200: '#757575',
-        100: '#B2B2B2'
-      }
+        100: "#B2B2B2", // Disabled
+        200: "#757575", // Secondary
+        300: "#333333", // Primary
+      },
+      light: {
+        100: "#",
+        200: "#",
+        300: "#",
+      },
     },
     link: {
-      100: '#', //normal
-      200: '#' //hover
+      100: "#", // normal
+      200: "#", // hover
+      300: "#", // active
     },
-    theme: {
-      // Brand color
-      brand: {
-        100: '#F5AA92', // light / background / disabled
-        200: '#EB5424', // default / background
-        300: '#D94514', // dark / hover
-        400: '#BF3A11' // darker / active
-      },
-      // Azul
+    // Active color
+    active: {
+      100: "#",
+      200: "#",
+      300: "#",
+    },
+    variant: {
       primary: {
-        100: '#', // light / disabled
-        200: '#', // default / background
-        300: '#', // dark / hover
-        400: '' // darker / active
+        100: "#", // light / disabled
+        200: "#", // default / background
+        300: "#", // dark / hover
+        400: "#", // darker / active
       },
-      // Status?
-      info: {
-        100: '#',
-        200: '#',
-        300: '#'
+      brand: {
+        100: "#F5AA92", // light / background disabled
+        200: "#EB5424", // default / background
+        300: "#D94514", // dark / hover
+        400: "#BF3A11", // darker / active
+      },
+      default: {
+        100: "#",
+        200: "#",
+        300: "#",
+        400: "#",
       },
       success: {
-        100: '#',
-        200: '#',
-        300: '#'
+        100: "#",
+        200: "#",
+        300: "#",
+      },
+      // Info / Primary are now the same
+      info: {
+        100: "#",
+        200: "#",
+        300: "#",
       },
       warning: {
-        100: '#',
-        200: '#',
-        300: '#'
+        100: "#",
+        200: "#",
+        300: "#",
       },
+      //
       danger: {
-        100: '#FFD8CC', // light / disabled
-        200: '#E13A37', // default / background
-        300: '#E40002', // dark
-        400: '#BE0001' // darker / text color
-      }
-    },
-    state: {
-      active: {
-        100: '#',
-        200: '#',
-        300: '#'
+        100: "#FFD8CC", // light / disabled
+        200: "#E13A37", // default / background
+        300: "#E40002", // dark
+        400: "#BE0001", // darker / text color
       },
-      disabled: {
-        100: '#',
-        200: '#',
-        300: '#'
-      }
-    }
+    },
   },
-
   // Borders ==========================
   border: {
     width: {
-      small: '1px',
-      medium: '2px'
-      // lg: '4px'
+      small: "1px",
+      medium: "2px",
+      // large: '4px'
     },
     radius: {
-      small: '',
-      medium: '3px', // this is the default
-      large: '30rem' // generated rounded corners
-    }
+      small: "",
+      medium: "3px", // this is the default
+      large: "",
+      round: "30rem", // generated rounded corners
+    },
   },
 
   // Shadows ==========================
+  // TDB
   shadow: {
-    inset: '',
-    small: '',
-    medium: '', // this is the default
-    large: ''
+    inset: "",
+    small: "",
+    medium: "", // this is the default
+    large: "",
   },
 
   /* Transparency
   // ========================== */
-
-  // ?????????
+  // TDB
 
   /* Sizes & spaces
   // ========================== */
@@ -171,29 +167,29 @@ const global = {
   // Used for for padding, margins, and position coordinates.
 
   spacing: {
-    xxsmall: '4px',
-    xsmall: '8px',
-    small: '12px',
-    medium: '16px', // this is the default
-    large: '24px',
-    xlarge: '32px',
-    '2xl': '40px',
-    '3xl': '56px'
-  },
-
-  // Size: Used for inputs/buttons height
-  size: {
-    xsmall: '32px',
-    small: '36px',
-    medium: '44px', // this is the default
-    large: '48px'
+    xxsmall: "4px",
+    xsmall: "8px",
+    small: "12px", // new value added
+    medium: "16px", // this is the default
+    large: "24px",
+    xlarge: "32px",
+    xxlarge: "40px",
+    xxxlarge: "56px",
   },
 
   gutter: {
-    xsmall: '8px',
-    small: '16px',
-    medium: '24px', // this is the default
-    large: '40px'
+    xsmall: "8px",
+    small: "16px",
+    medium: "24px", // this is the default
+    large: "40px",
+  },
+
+  // Size: Used for inputs/buttons height
+  height: {
+    xsmall: "32px",
+    small: "36px",
+    medium: "44px", // this is the default
+    large: "48px",
   },
 
   // Fonts ==========================
@@ -201,57 +197,57 @@ const global = {
     family: {
       sans:
         'fakt-web, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"', // Body and Headings
-      monospace: 'Roboto Mono, "Courier New", Courier, monospace' // Code
+      monospace: 'Roboto Mono, "Courier New", Courier, monospace', // Code
       // serif: '' // Blog
     },
     weight: {
-      light: '300',
-      normal: '400',
-      semiBold: '500',
-      bold: '700'
+      light: "300",
+      normal: "400",
+      medium: "500",
+      bold: "700",
     },
     size: {
-      xs: '',
-      sm: '',
-      md: '', // this is the default
-      lg: '',
-      xl: '',
-      '2xl': '',
-      '3xl': '',
-      '4xl': ''
+      xsmall: "",
+      small: "",
+      medium: "", // this is the default
+      large: "",
+      xlarge: "",
+      xxlarge: "",
+      xxxlarge: "",
     },
     sizeRelative: {
-      smaller: '90%'
+      smaller: "90%",
     },
     lineHeight: {
-      sm: '1.3',
-      md: '1.6' // this is the default
+      small: "1.3",
+      medium: "1.6", // this is the default
     },
     letterSpacing: {
-      large: '1px'
-    }
+      large: "1px",
+    },
   },
 
   // Breakpoint ==========================
   breakpoint: {
-    xs: '0',
-    sm: '576px',
-    md: '768px',
-    lg: '992px',
-    xl: '1200px'
+    xsmall: "0",
+    small: "576px",
+    medium: "768px",
+    large: "992px",
+    xlarge: "1200px",
   },
 
   // Z-Index ==========================
   zindex: {
-    xs: '100',
-    sm: '200', // Tooltip, dropmenus
-    md: '300',
-    lg: '400', // App header
-    xl: '500', // Clippo
-    '2xl': '600' // Modal overlay
-  }
-  //Animations
-  //TDB
-}
+    xsmall: "100",
+    small: "200", // Tooltip, dropmenus
+    medium: "300",
+    large: "400", // App header
+    xlarge: "500", // Clippo
+    xxlarge: "600", // Modal overlay
+  },
 
-module.exports = global
+  // Animations ==========================
+  // TDB
+};
+
+module.exports = global;
--- core/tokens/v2/swatches.js
+++ core/tokens/v2/swatches.js
@@ -1,23 +1,35 @@
 const swatch = {
-  // Colors
-  white: '#ffffff',
-  'neutral-100': '#fafafa',
-  'neutral-200': '#ededed',
-  'neutral-300': '#d1d1d1',
-  'neutral-400': '#bbb',
-  'neutral-500': '#8b8d8f',
-  'neutral-600': '#72767b',
-  'neutral-700': '#4d5258',
-  'neutral-800': '#393f44',
-  'neutral-900': '#282d33',
-  black: '#000000',
-  'blue-100': '#bee1f4',
-  'gold-100': '#fbeabc',
-  'green-100': '#a2da9c',
-  'light-blue-100': '#beedf9',
-  'light-green-100': '#e4f5bc',
-  'orange-100': '#f4b678',
-  'red-100': '#c00'
-}
+  // Neutrals
+  // *********
+  white: "#ffffff",
+  "grey-100": "#fafafa",
+  "grey-200": "#ededed",
+  "grey-300": "#d1d1d1",
+  "grey-400": "#bbb",
+  "grey-500": "#8b8d8f",
+  "grey-600": "#72767b",
+  "grey-700": "#4d5258",
+  "grey-800": "#393f44",
+  "grey-900": "#282d33",
+  black: "#333333",
+  // Red
+  // *********
+  "red-100": "#c00",
+  // Orange
+  // *********
+  "orange-100": "#f4b678",
+  // Gold
+  // *********
+  "gold-100": "#fbeabc",
+  // Green
+  // *********
+  "green-100": "#a2da9c",
+  // Light-green (Lime)
+  // ************
+  "light-green-100": "#e4f5bc",
+  // Light Blue ()
+  // *********
+  "light-blue-100": "#beedf9",
+};
 
-module.exports = swatches
+module.exports = swatches;
