e0114a83687853a173c4b36cdab8b86da868723b
Fernando Carrettoni <fcarrettoni@gmail.com>
2020-01-17 10:47:08 -0300

    Improved structure

--- core/tokens/v2/global.js
+++ core/tokens/v2/global.js
@@ -77,58 +77,79 @@ const global = {
         300: "#",
       },
     },
+    border: {
+      100: "#",
+      200: "#",
+      300: "#",
+    },
     link: {
       100: "#", // normal
       200: "#", // hover
       300: "#", // active
     },
-    // Active color
-    active: {
-      100: "#",
-      200: "#",
-      300: "#",
-    },
-    variant: {
+    appearance: {
       primary: {
-        100: "#", // light / disabled
-        200: "#", // default / background
-        300: "#", // dark / hover
-        400: "#", // darker / active
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "light-blue-600", // dark
+        900: "", // darker
       },
       brand: {
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
+        /*
         100: "#F5AA92", // light / background disabled
         200: "#EB5424", // default / background
         300: "#D94514", // dark / hover
         400: "#BF3A11", // darker / active
+        */
       },
       default: {
-        100: "#",
-        200: "#",
-        300: "#",
-        400: "#",
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
       },
       success: {
-        100: "#",
-        200: "#",
-        300: "#",
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
       },
       // Info / Primary are now the same
       info: {
-        100: "#",
-        200: "#",
-        300: "#",
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
       },
       warning: {
-        100: "#",
-        200: "#",
-        300: "#",
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
       },
       //
       danger: {
+        100: "#", // lighter
+        300: "#", // light
+        500: "#", // default
+        700: "#", // dark
+        900: "", // darker
+        /*
         100: "#FFD8CC", // light / disabled
         200: "#E13A37", // default / background
         300: "#E40002", // dark
         400: "#BE0001", // darker / text color
+        */
       },
     },
   },
@@ -165,7 +186,6 @@ const global = {
 
   // Spacing:
   // Used for for padding, margins, and position coordinates.
-
   spacing: {
     xxsmall: "4px",
     xsmall: "8px",
@@ -176,14 +196,12 @@ const global = {
     xxlarge: "40px",
     xxxlarge: "56px",
   },
-
   gutter: {
     xsmall: "8px",
     small: "16px",
     medium: "24px", // this is the default
     large: "40px",
   },
-
   // Size: Used for inputs/buttons height
   height: {
     xsmall: "32px",
--- core/tokens/v2/swatches.js
+++ core/tokens/v2/swatches.js
@@ -12,24 +12,56 @@ const swatch = {
   "grey-800": "#393f44",
   "grey-900": "#282d33",
   black: "#333333",
-  // Red
-  // *********
-  "red-100": "#c00",
   // Orange
   // *********
   "orange-100": "#f4b678",
-  // Gold
-  // *********
-  "gold-100": "#fbeabc",
-  // Green
-  // *********
-  "green-100": "#a2da9c",
-  // Light-green (Lime)
-  // ************
-  "light-green-100": "#e4f5bc",
+  "orange-200": "#",
+  "orange-300": "#",
+  "orange-400": "#",
+  "orange-500": "#",
+  "orange-600": "#",
+  "orange-700": "#",
+  "orange-800": "#",
+  "orange-900": "#",
   // Light Blue ()
   // *********
   "light-blue-100": "#beedf9",
+  "light-blue-200": "#",
+  "light-blue-300": "#",
+  "light-blue-400": "#",
+  "light-blue-500": "#",
+  "light-blue-600": "#",
+  "light-blue-700": "#",
+  "light-blue-800": "#",
+  "light-blue-900": "#",
+  // Gold
+  // *********
+  "gold-100": "#fbeabc",
+  "gold-300": "#",
+  "gold-500": "#",
+  "gold-700": "#",
+  "gold-900": "#",
+  // Green
+  // *********
+  "green-100": "#a2da9c",
+  "green-300": "#",
+  "green-500": "#",
+  "green-700": "#",
+  "green-900": "#",
+  // Light-green (Lime)
+  // ************
+  "light-green-100": "#e4f5bc",
+  "light-green-300": "",
+  "light-green-500": "",
+  "light-green-700": "",
+  "light-green-900": "",
+  // Red
+  // *********
+  "red-100": "#c00",
+  "red-300": "#",
+  "red-500": "#",
+  "red-700": "#",
+  "red-900": "#",
 };
 
 module.exports = swatches;
