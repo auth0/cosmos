6d35671b69efb03512879e56b1ab7422b719d80c
loginist <pancho.auth0@gmail.com>
2020-01-29 14:40:03 -0300

    Prettier changes

--- core/components/molecules/form/form.tsx
+++ core/components/molecules/form/form.tsx
@@ -1,61 +1,61 @@
-import * as React from 'react'
-import styled from '../../styled'
-import Automation from '../../_helpers/automation-attribute'
+import * as React from "react";
+import styled from "../../styled";
+import Automation from "../../_helpers/automation-attribute";
 
-import ExtTextInput from '../../atoms/text-input'
-import ExtTextArea from '../../atoms/textarea'
-import ExtSelect from '../../atoms/select'
-import ExtSwitch from '../../atoms/switch'
-import ExtRadio from '../../atoms/radio'
+import ExtTextInput from "../../atoms/text-input";
+import ExtTextArea from "../../atoms/textarea";
+import ExtSelect from "../../atoms/select";
+import ExtSwitch from "../../atoms/switch";
+import ExtRadio from "../../atoms/radio";
 
-import ExtField from './field'
-import ExtActions from './actions'
-import ExtFieldSet from './fieldset'
+import ExtField from "./field";
+import ExtActions from "./actions";
+import ExtFieldSet from "./fieldset";
 
-import FormContext from './form-context'
+import FormContext from "./form-context";
 
 export interface IFormProps {
   /** HTML ID of the component */
-  id?: string
+  id?: string;
   /** Two options for controlling form layout */
-  layout?: 'label-on-left' | 'label-on-top'
+  layout?: "label-on-left" | "label-on-top";
   /** Remove the 625px constraint from the form fields width */
-  fullWidth?: boolean
-  children?: React.ReactNode
-  onSubmit?: Function
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onSubmit?: Function;
 }
 
 const Form = (props: IFormProps) => (
   <FormContext.Provider value={{ layout: props.layout, fullWidth: props.fullWidth }}>
-    <Form.Element {...props} {...Automation('form')} />
+    <Form.Element {...props} {...Automation("form")} />
   </FormContext.Provider>
-)
+);
 
-Form.Field = ExtField
-Form.TextInput = (props) => <ExtField {...props} fieldComponent={ExtTextInput} />
-Form.TextArea = (props) => <ExtField {...props} fieldComponent={ExtTextArea} />
-Form.Select = (props) => <ExtField {...props} fieldComponent={ExtSelect} />
-Form.Switch = (props) => <ExtField {...props} fieldComponent={ExtSwitch} />
+Form.Field = ExtField;
+Form.TextInput = (props) => <ExtField {...props} fieldComponent={ExtTextInput} />;
+Form.TextArea = (props) => <ExtField {...props} fieldComponent={ExtTextArea} />;
+Form.Select = (props) => <ExtField {...props} fieldComponent={ExtSelect} />;
+Form.Switch = (props) => <ExtField {...props} fieldComponent={ExtSwitch} />;
 
 const FormRadio: { (props): JSX.Element; Option: any } = (() => {
-  const Radio = (props) => <ExtField {...props} fieldComponent={ExtRadio} />
-  Radio.Option = ExtRadio.Option
-  return Radio
-})()
+  const Radio = (props) => <ExtField {...props} fieldComponent={ExtRadio} />;
+  Radio.Option = ExtRadio.Option;
+  return Radio;
+})();
 
-Form.Radio = FormRadio
+Form.Radio = FormRadio;
 
-Form.Actions = ExtActions
-Form.FieldSet = ExtFieldSet
+Form.Actions = ExtActions;
+Form.FieldSet = ExtFieldSet;
 
 Form.Element = styled.form`
   ${ExtField.Element}, ${ExtActions.Element} {
-    max-width: ${(props) => (props.layout === 'label-on-top' || props.fullWidth ? 'auto' : '624px')};
+    max-width: ${(props) => (props.layout === "label-on-top" || props.fullWidth ? "auto" : "624px")};
   }
-`
+`;
 
 Form.defaultProps = {
-  layout: 'label-on-left'
-}
+  layout: "label-on-left"
+};
 
-export default Form
+export default Form;
