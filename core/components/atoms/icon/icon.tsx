be7419af874b952f2304829ddf68ddd3f4b0b105
loginist <pancho.auth0@gmail.com>
2020-01-30 16:21:33 -0300

    Prettier

--- core/components/atoms/icon/icon.tsx
+++ core/components/atoms/icon/icon.tsx
@@ -1,53 +1,48 @@
-import * as React from 'react'
-import styled from '../../styled'
+import * as React from "react";
+import styled from "../../styled";
 
-import { colors } from '../../tokens'
-import iconData from './icons.json'
-import Automation from '../../_helpers/automation-attribute'
+import { colors } from "../../tokens";
+import iconData from "./icons.json";
+import Automation from "../../_helpers/automation-attribute";
 
 export interface IIconProps {
   /** HTML ID of the component */
-  id?: string
+  id?: string;
   /** Icon name */
-  name: string
+  name: string;
   /** Icon size */
-  size?: number | string
+  size?: number | string;
   /** Icon foreground color */
-  color?: string
-  onClick?: Function
+  color?: string;
+  onClick?: Function;
 }
 
-const icons = iconData.icons
+const icons = iconData.icons;
 
 const Icon = (props: IIconProps) => {
   // If the icon name isn't found, show a question mark instead.
-  const icon = icons[props.name] || icons.help
+  const icon = icons[props.name] || icons.help;
 
   // Use an icon name from tokens, fallback to hexcode (without validation)
-  const color = colors.base[props.color] || props.color
+  const color = colors.base[props.color] || props.color;
 
   return (
-    <Icon.Element {...Automation('icon')} {...props}>
-      <Image
-        width={props.size}
-        height={props.size}
-        viewBox={`0 0 ${icon.width} ${icon.height}`}
-        color={color}
-      >
+    <Icon.Element {...Automation("icon")} {...props}>
+      <Image width={props.size} height={props.size} viewBox={`0 0 ${icon.width} ${icon.height}`} color={color}>
         {icon.paths.map((path, index) => (
           <path key={index} d={path} />
         ))}
       </Image>
     </Icon.Element>
-  )
-}
+  );
+};
 
 Icon.Element = styled.i`
   display: inline-block;
   line-height: 1;
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
-`
+`;
 const Image = styled.svg`
   display: inline-block;
   vertical-align: middle;
@@ -57,15 +52,15 @@ const Image = styled.svg`
   path {
     fill: ${(imageProps) => imageProps.color};
   }
-`
+`;
 
-const __ICONNAMES__ = Object.keys(icons)
-const __COLORS__ = Object.keys(colors.base)
+const __ICONNAMES__ = Object.keys(icons);
+const __COLORS__ = Object.keys(colors.base);
 
 Icon.defaultProps = {
   size: 20,
-  color: 'default'
-}
+  color: "default"
+};
 
-export default Icon
-export { __ICONNAMES__ }
+export default Icon;
+export { __ICONNAMES__ };
