f87a241a5aea46c5dbb43856952ea17c259ac955
Pancho <50875398+loginist@users.noreply.github.com>
2020-02-19 14:13:43 -0300

    Fix: Deprecation notice in error prop comment (#1705)

--- core/components/atoms/text-input/text-input.tsx
+++ core/components/atoms/text-input/text-input.tsx
@@ -1,62 +1,62 @@
-import * as React from 'react'
-import styled from '../../styled'
+import * as React from "react";
+import styled from "../../styled";
 
-import { misc } from '../../tokens'
-import { StyledInput } from '../_styled-input'
-import Automation from '../../_helpers/automation-attribute'
-import InputWithActions from '../_input-with-actions'
-import Form from '../../molecules/form'
-import { ActionWithIcon } from '../../_helpers/action-shape'
+import { misc } from "../../tokens";
+import { StyledInput } from "../_styled-input";
+import Automation from "../../_helpers/automation-attribute";
+import InputWithActions from "../_input-with-actions";
+import Form from "../../molecules/form";
+import { ActionWithIcon } from "../../_helpers/action-shape";
 
-export type TextInputSize = 'default' | 'large' | 'small' | 'compressed'
+export type TextInputSize = "default" | "large" | "small" | "compressed";
 
 export interface ITextInputProps {
   /** HTML ID for the element */
-  id?: string
+  id?: string;
   /** HTML name for the element */
-  name?: string
+  name?: string;
   /** Hide input similar to passwords but for other private information. Implies readOnly. */
-  masked?: boolean
+  masked?: boolean;
   /** Make input readOnly if it does not validate constraint */
-  readOnly?: boolean
+  readOnly?: boolean;
   /** Use when the expected input is code */
-  code?: boolean
+  code?: boolean;
   /** Pass hasError to show error state */
-  hasError?: boolean
-  /** @deprecated:hasError Pass error string directly to show error state */
-  error?: string
+  hasError?: boolean;
+  /** @deprecated use hasError instead */
+  error?: string;
   /** onChange transparently passed to the input */
-  onChange?: Function
+  onChange?: Function;
   /** Text to display when the input is empty */
-  placeholder?: string
+  placeholder?: string;
   /** The default value for the field */
-  defaultValue?: string
+  defaultValue?: string;
   /** The current value for the field */
-  value?: string
+  value?: string;
   /** The (HTML) type for the input. */
-  type?: string
+  type?: string;
   /** The size of the input. */
-  size?: TextInputSize
+  size?: TextInputSize;
   /** The (HTML) label for the input. */
-  label?: string
+  label?: string;
   /** Actions to be attached to the input */
-  actions?: Array<React.ReactNode | ActionWithIcon>
-  onClick?: Function
-  onBlur?: Function
-  onFocus?: Function
+  actions?: Array<React.ReactNode | ActionWithIcon>;
+  onClick?: Function;
+  onBlur?: Function;
+  onFocus?: Function;
   /** Native HTML pattern property, accepts a regular expression to validate the input */
-  pattern?: string
-  disabled?: boolean
-  min?: number
-  max?: number
-  autoFocus?: boolean
-  autoComplete?: string
-  autoCorrect?: string
-  spellCheck?: boolean
+  pattern?: string;
+  disabled?: boolean;
+  min?: number;
+  max?: number;
+  autoFocus?: boolean;
+  autoComplete?: string;
+  autoCorrect?: string;
+  spellCheck?: boolean;
 }
 
 const TextInput = (props: ITextInputProps) => {
-  let { defaultValue, placeholder, readOnly, ...restOfTheProps } = props
+  let { defaultValue, placeholder, readOnly, ...restOfTheProps } = props;
 
   /*
     override placeholder and readOnly for masked
@@ -65,10 +65,10 @@ const TextInput = (props: ITextInputProps) => {
     (like password, but without the value underneath)
   */
   if (props.masked) {
-    const length = props.defaultValue ? props.defaultValue.length : 8
-    placeholder = new Array(length).join('•')
-    readOnly = true
-    defaultValue = null
+    const length = props.defaultValue ? props.defaultValue.length : 8;
+    placeholder = new Array(length).join("•");
+    readOnly = true;
+    defaultValue = null;
   }
 
   /*
@@ -83,7 +83,7 @@ const TextInput = (props: ITextInputProps) => {
     <Form.Field.ContextConsumer>
       {(context) => (
         <TextInput.Element
-          {...Automation('text-input')}
+          {...Automation("text-input")}
           id={props.id || context.formFieldId}
           defaultValue={defaultValue}
           placeholder={placeholder}
@@ -92,32 +92,33 @@ const TextInput = (props: ITextInputProps) => {
         />
       )}
     </Form.Field.ContextConsumer>
-  )
+  );
 
-  if (!props.actions.length) { return Input }
-  else {
+  if (!props.actions.length) {
+    return Input;
+  } else {
     /* Input is not a component, just JSX, hence wrapper in {} */
     return (
       <InputWithActions actions={props.actions} size={props.size}>
         {Input}
       </InputWithActions>
-    )
+    );
   }
-}
+};
 
 TextInput.Element = styled(StyledInput)`
   height: ${(props) => misc.input[props.size].height};
-`
+`;
 
 TextInput.defaultProps = {
   readOnly: false,
   code: false,
   error: null,
   onChange: null,
-  type: 'text',
-  size: 'default',
+  type: "text",
+  size: "default",
   actions: []
-}
+};
 
-export default TextInput
-export { StyledInput }
+export default TextInput;
+export { StyledInput };
