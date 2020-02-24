914ad6111255ee456f3f0a39f15222abee9d5880
Doug Miller <doug@dougmiller.io>
2020-02-24 10:54:12 -0500

    Release v0.35.0

--- internal/docs/guides.js
+++ internal/docs/guides.js
@@ -1,19 +1,21 @@
-import GuidingPrinciples from './guides/guiding-principles'
-import Usage from './guides/usage'
-import ContributionGuide from './guides/contribution-guide'
-import FAQS from './guides/faqs'
-import Changes from './guides/changes'
-import MigrationGuide from './guides/migration-guide'
-import GettingStarted from './guides/getting-started'
+import GuidingPrinciples from "./guides/guiding-principles";
+import Usage from "./guides/usage";
+import ContributionGuide from "./guides/contribution-guide";
+import FAQS from "./guides/faqs";
+import Changes from "./guides/changes";
+import MigrationGuide from "./guides/migration-guide";
+import GettingStarted from "./guides/getting-started";
+import UpgradeGuide from "./guides/upgrade-guide";
 
 const guides = [
-  { path: '/', title: 'Getting started', component: GettingStarted },
-  { path: '/usage', title: 'How to use Cosmos?', component: Usage },
-  { path: '/guiding-principles', title: 'Guiding Principles', component: GuidingPrinciples },
-  { path: '/faqs', title: 'FAQs', component: FAQS },
-  { path: '/changes', title: 'Changelog', component: Changes },
-  { path: '/contribution-guide', title: 'Contributing to Cosmos', component: ContributionGuide },
-  { path: '/migration-guide', title: 'Migration Guide', component: MigrationGuide }
-]
+  { path: "/", title: "Getting started", component: GettingStarted },
+  { path: "/usage", title: "How to use Cosmos?", component: Usage },
+  { path: "/guiding-principles", title: "Guiding Principles", component: GuidingPrinciples },
+  { path: "/faqs", title: "FAQs", component: FAQS },
+  { path: "/changes", title: "Changelog", component: Changes },
+  { path: "/contribution-guide", title: "Contributing to Cosmos", component: ContributionGuide },
+  { path: "/migration-guide", title: "Migration Guide", component: MigrationGuide },
+  { path: "/upgrade-guide", title: "Upgrade Guide", component: UpgradeGuide }
+];
 
-export default guides
+export default guides;
